342b84baa9aaadcdbcc0e9e9cbeafb05fc4de074
Thomas Rampelberg <thomas@saunter.org>
2014-11-19 10:58:20 -0800

    adding in the fake framework

--- dcos/registry.py
+++ dcos/registry.py
@@ -17,6 +17,62 @@ def list():
         OrderedDict([
             ("name", "spark"),
             ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "casandra"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "kafka"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "kubernetes"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "chronos"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "jenkins"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "spark"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "HDFS"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "DEIS"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "hadoop"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "yarn"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "accumulo"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "ElasticSearch"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "Aurora"),
+            ("version", "0.1.1")
+        ]),
+        OrderedDict([
+            ("name", "Storm"),
+            ("version", "0.1.1")
         ])
     ]
 
