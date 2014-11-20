ad1985c33a7f7c51237c9cced51bd67b42d1ab9f
Thomas Rampelberg <thomas@saunter.org>
2014-11-19 17:47:55 -0800

    add nodes with fake cassandra

--- dcos/cmds/cassandra/add.py
+++ dcos/cmds/cassandra/add.py
@@ -1,6 +1,11 @@
 
 from __future__ import absolute_import, print_function
 
+import copy
+import json
+import requests
+import urlparse
+
 from ... import cli
 from ... import service
 
@@ -12,6 +17,16 @@ parser.add_argument(
     "number", type=int, help="number of nodes to add"
 )
 
+NODE_CONFIG = {
+    "mem": 10,
+    "cpus": 0.1
+}
+
+def start_tasks(url, cfg, t="long"):
+    return requests.post(urlparse.urljoin(url, t), data=json.dumps(cfg)).text
+
 @cli.init(parser)
 def main(args):
-    print(service.find("cassandra"))
+    cfg = copy.copy(NODE_CONFIG)
+    cfg["num"] = args.number
+    print(start_tasks(service.find("cassandra"), cfg))
