2ff79c9299ca8cb045088a56c547d7895e102062
Marco Massenzio <m.massenzio@gmail.com>
2015-05-30 17:49:29 -0700

    Attempts to use venv 3.4 if virtualenv version installed is too ancient

--- bin/install/install-dcos-cli.sh
+++ bin/install/install-dcos-cli.sh
@@ -14,18 +14,29 @@ fi
 
 ARGS=( "$@" );
 
+VENV="virtualenv"
 VIRTUAL_ENV_PATH=$(python -c "import os; print(os.path.realpath('"${ARGS[0]}"'))")
 DCOS_URL=${ARGS[1]}
 
-command -v virtualenv >/dev/null 2>&1 || { echo "Cannot find virtualenv. Aborting."; exit 1; }
+IS_VENV=$(which ${VENV})
+if [[ -z "${IS_VENV}" ]]; then
+    echo "Cannot find virtualenv. Aborting."
+    exit 1
+fi
 
-VIRTUALENV_VERSION=$(virtualenv --version)
+VIRTUALENV_VERSION=$(${VENV} --version)
 VERSION_REGEX="s#[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)#\1#"
 
 eval MAJOR=`echo $VIRTUALENV_VERSION | sed -e $VERSION_REGEX`
-if [ $MAJOR -lt 12 ];
-	then echo "Virtualenv version must be 12 or greater. Aborting.";
-	exit 1;
+if [ $MAJOR -lt 12 ]; then
+    # On some (most?) Linux distro, venv for 2.7 is an ancient 1.11.6
+    # Let's try and see if we can find virtualenv-3.4 and use that one instead
+    VENV=$(which virtualvenv-3.4)
+    if [[ -z ${VENV} ]]; then
+        echo "Virtualenv version must be 12 or greater."
+        echo "You can upgrade by running: sudo pip install --upgrade virtualenv"
+        exit 1
+    fi
 fi
 
 echo "Installing DCOS CLI from PyPI...";
@@ -33,7 +44,7 @@ echo "";
 
 # Let's first setup a virtualenv: we are assuming that the path is absolute
 mkdir -p "$VIRTUAL_ENV_PATH"
-virtualenv "$VIRTUAL_ENV_PATH"
+${VENV} "$VIRTUAL_ENV_PATH"
 
 # Install the DCOS CLI package, using version if set
 if [ -z "$DCOS_CLI_VERSION" ]; then
