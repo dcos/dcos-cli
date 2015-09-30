17d1ac42d2619d7b8a372423ffc16e5d9496f4d0
Michael Gummelt <mgummelt@mesosphere.io>
2015-09-30 15:26:07 -0700

    .

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -2,9 +2,14 @@
 
 # You must set DCOS_CLI_VERSION to run this program
 
+if [ -z "$DCOS_CLI_VERSION" ]; then
+    echo "Please set the DCOS_CLI_VERSION env var."
+    exit 1;
+fi
+
 set -e -x
 
-sed -i -e "s/\$dcos_cli_version/${DCOS_CLI_VERSION}/g" docker/Dockerfile
+sed -i -e "s/\$docker_cli_version/${DCOS_CLI_VERSION}/g" docker/Dockerfile
 
 pushd docker
 
