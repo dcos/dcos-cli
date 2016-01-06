24e6bdaa201e4816236b2eb65e4ffd0a7a5ea82b
Tamar Ben-Shachar <tamar@mesosphere.io>
2016-01-05 17:07:33 -0800

    add cosmos env variable

--- dcos/constants.py
+++ dcos/constants.py
@@ -31,3 +31,6 @@ DCOS_COMMAND_PREFIX = 'dcos-'
 
 VALID_LOG_LEVEL_VALUES = ['debug', 'info', 'warning', 'error', 'critical']
 """List of all the supported log level values for the CLIs"""
+
+COSMOS_URL_ENV = 'COSMOS_URL'
+"""Name of the environment variable pointing to URL of cosmos server"""
