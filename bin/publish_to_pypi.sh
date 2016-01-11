59d76e7f1403c414f816d6e0f0b863b82a6fc022
José Armando García Sancio <jsancio@gmail.com>
2016-01-10 21:34:30 -0800

    Fix publish to pypi script

--- bin/publish_to_pypi.sh
+++ bin/publish_to_pypi.sh
@@ -10,10 +10,10 @@ cat <<EOF > ~/.pypirc
 index-servers =
     pypi
 
-    [pypi]
-    repository: https://pypi.python.org/pypi
-    username:$PYPI_USERNAME
-    password:$PYPI_PASSWORD
+[pypi]
+repository: https://pypi.python.org/pypi
+username:$PYPI_USERNAME
+password:$PYPI_PASSWORD
 EOF
 
 # replace SNAPSHOT with tagged version
