e1e0fba7a3184d7237c7e5b9d1dd7d27eb096c56
tamarrow <tamar@mesosphere.io>
2016-05-19 11:07:31 -0700

    update make scripts to be compatiable with windows (#617)

--- win_bin/packages.ps1
+++
@@ -1,7 +0,0 @@
-﻿$BaseDir = (Get-Location).Path
-
-echo "Building wheel..."
-& "$BASEDIR\env\Scripts\python.exe" setup.py bdist_wheel
-
-echo "Building egg..."
-& "$BASEDIR\env\Scripts\python.exe" setup.py sdist
