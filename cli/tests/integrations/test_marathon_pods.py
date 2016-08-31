030ef7607de994e0ca250bd23aa1a5d26f143642
Charles Ruhland <charles@mesosphere.io>
2016-08-30 18:59:51 -0700

    WIP: add `pod update` tests

--- cli/tests/integrations/test_marathon_pods.py
+++ cli/tests/integrations/test_marathon_pods.py
@@ -1,7 +1,11 @@
+import contextlib
+import re
+
 from .common import exec_command
 
 GOOD_POD_ID = 'good-pod'
 GOOD_POD_FILE_PATH = 'tests/data/marathon/pods/good.json'
+UPDATED_GOOD_POD_FILE_PATH = 'tests/data/marathon/pods/updated_good.json'
 
 
 def test_pod_add_from_file_then_remove():
@@ -31,34 +35,49 @@ def test_pod_list_table():
 
 
 def test_pod_show():
-    exit_status = _pod_add_from_file(GOOD_POD_FILE_PATH)
-    assert exit_status == 0
+    with _pod(GOOD_POD_ID, GOOD_POD_FILE_PATH, add_fn=_pod_add_from_file):
+        exit_status, stdout = _pod_show(GOOD_POD_ID)
+        assert exit_status == 0
 
-    exit_status, stdout = _pod_show(GOOD_POD_ID)
-    assert exit_status == 0
-
-    good_pod_json = _read_file(GOOD_POD_FILE_PATH)
-    assert stdout == good_pod_json
+        good_pod_json = _read_file(GOOD_POD_FILE_PATH)
+        assert stdout == good_pod_json
 
 
 def test_pod_update_from_file():
-    # Add pod, verify that it can be updated
-    pass
+    with _pod(GOOD_POD_ID, GOOD_POD_FILE_PATH, add_fn=_pod_add_from_file):
+        exit_status, stdout = _pod_update_from_file(GOOD_POD_ID,
+                                                    UPDATED_GOOD_POD_FILE_PATH,
+                                                    force=False)
+        assert exit_status == 0
+        assert re.fullmatch('Created deployment \S+\n', stdout.decode('utf-8'))
+
+        exit_status, stdout = _pod_show(GOOD_POD_ID)
+        assert exit_status == 0
+
+        updated_good_pod_json = _read_file(UPDATED_GOOD_POD_FILE_PATH)
+        assert stdout == updated_good_pod_json
 
 
-def test_pod_update_from_file_force_true():
-    # Add pod, verify that it can be --force updated
-    pass
+def test_pod_update_from_stdin_force_true():
+    with _pod(GOOD_POD_ID, GOOD_POD_FILE_PATH, add_fn=_pod_add_from_stdin):
+        exit_status, stdout = _pod_update_from_file(GOOD_POD_ID,
+                                                    UPDATED_GOOD_POD_FILE_PATH,
+                                                    force=True)
+        assert exit_status == 0
+        assert re.fullmatch('Created deployment \S+\n', stdout.decode('utf-8'))
 
+        exit_status, stdout = _pod_show(GOOD_POD_ID)
+        assert exit_status == 0
 
-def test_pod_update_from_stdin():
-    # Add pod, verify that it can be updated from spec on stdin
-    pass
+        updated_good_pod_json = _read_file(UPDATED_GOOD_POD_FILE_PATH)
+        assert stdout == updated_good_pod_json
+
 
 _POD_BASE_CMD = ['dcos', 'marathon', 'pod']
 _POD_ADD_CMD = _POD_BASE_CMD + ['add']
 _POD_REMOVE_CMD = _POD_BASE_CMD + ['remove']
 _POD_SHOW_CMD = _POD_BASE_CMD + ['show']
+_POD_UPDATE_CMD = _POD_BASE_CMD + ['update']
 
 
 def _pod_add_from_file(file_path):
@@ -75,8 +94,7 @@ def _pod_add_from_stdin(file_path):
 
 
 def _pod_remove(pod_id, force):
-    force_arg = ['--force'] if force else []
-    cmd = _POD_REMOVE_CMD + force_arg + [pod_id]
+    cmd = _POD_REMOVE_CMD + _force_args(force) + [pod_id]
     exit_status, stdout, stderr = exec_command(cmd)
     return exit_status
 
@@ -87,6 +105,30 @@ def _pod_show(pod_id):
     return exit_status, stdout
 
 
+def _pod_update_from_file(pod_id, file_path, force):
+    cmd = _POD_UPDATE_CMD + _force_args(force) + [pod_id, file_path]
+    exit_status, stdout, stderr = exec_command(cmd)
+    return exit_status, stdout
+
+
 def _read_file(file_path):
     with open(file_path) as fd:
         return fd.read()
+
+
+def _force_args(force):
+    return ['--force'] if force else []
+
+
+@contextlib.contextmanager
+def _pod(pod_id, file_path, add_fn):
+    exit_status = add_fn(file_path)
+    pod_added = (exit_status == 0)
+
+    try:
+        assert pod_added
+        yield
+    finally:
+        if pod_added:
+            exit_status = _pod_remove(pod_id, force=True)
+            assert exit_status == 0
