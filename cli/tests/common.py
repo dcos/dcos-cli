3d51a930ce5c82038f55da0f973b1bd0c6a9666c
Charles Ruhland <cruhland@alumni.cmu.edu>
2016-08-31 16:16:46 -0700

    Pods integration tests (#748)

---
+++ cli/tests/common.py
@@ -0,0 +1,18 @@
+
+def assert_same_elements(list1, list2):
+    """Tests whether the second argument is a rearrangement of the first.
+
+    This method will work even if the list elements are mutable and/or cannot
+    be sorted. A common use case is comparing lists of JSON objects, since
+    the objects are usually implemented with dicts.
+
+    :param list1: the first list
+    :type: list
+    :param list2: the second list
+    :type: list
+    :rtype: None
+    """
+    list2 = list2.copy()
+    for element in list1:
+        list2.remove(element)
+    assert list2 == []
