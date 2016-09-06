a1f2fa33d575d3d2bb411b83fbd1763b11ae9a0c
Charles Ruhland <charles@mesosphere.io>
2016-09-06 13:45:25 -0700

    Unit test for handling of Marathon responses with status code 400

--- tests/test_marathon.py
+++ tests/test_marathon.py
@@ -1,6 +1,9 @@
+import requests
 from dcos import http, marathon
+from dcos.errors import DCOSException, DCOSHTTPException
 
 import mock
+import pytest
 
 
 def test_add_pod_puts_json_in_request_body():
@@ -12,6 +15,24 @@ def test_add_pod_puts_json_in_request_body():
 def test_add_pod_returns_parsed_response_body():
     _assert_add_pod_returns_parsed_response_body({"id": "i-am-a-pod"})
     _assert_add_pod_returns_parsed_response_body(["another", "pod", "json"])
+
+
+def test_rpc_client_http_req_converts_exception_for_400_status():
+    request = requests.Request(method='FOO', url='http://request/url')
+    response = requests.Response()
+    response.request = request.prepare()
+    response.status_code = 400
+    response.reason = 'Something Bad'
+
+    def bad_method(*args, **kwargs):
+        raise DCOSHTTPException(response)
+
+    rpc_client = marathon.RpcClient('http://does/not/matter')
+    with pytest.raises(DCOSException) as exception_info:
+        rpc_client.http_req(method_fn=bad_method, path='arbitrary/path')
+
+    msg = 'Error on request [FOO http://request/url]: HTTP 400: Something Bad'
+    assert str(exception_info.value) == msg
 
 
 def _assert_add_pod_puts_json_in_request_body(pod_json):
