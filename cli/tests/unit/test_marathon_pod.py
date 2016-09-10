b8cb6891ebebc546165bd3a503f639304a25716c
Charles Ruhland <charles@mesosphere.io>
2016-09-09 22:31:15 -0700

    Added test case

--- cli/tests/unit/test_marathon_pod.py
+++ cli/tests/unit/test_marathon_pod.py
@@ -1,15 +1,22 @@
 import dcoscli.marathon.main as main
 from dcos import marathon
+from dcos.errors import DCOSException
 
+import pytest
 from mock import create_autospec
 
 
-def test_add_invoked_successfully():
-    _assert_add_invoked_successfully(pod_file_json={"arbitrary": "json"})
-    _assert_add_invoked_successfully(pod_file_json=["more", "json"])
+def test_pod_add_invoked_successfully():
+    _assert_pod_add_invoked_successfully(pod_file_json={"arbitrary": "json"})
+    _assert_pod_add_invoked_successfully(pod_file_json=["more", "json"])
 
 
-def _assert_add_invoked_successfully(pod_file_json):
+def test_pod_add_propagates_exceptions_from_add_pod():
+    _assert_pod_add_propagates_exceptions_from_add_pod(DCOSException('BOOM!'))
+    _assert_pod_add_propagates_exceptions_from_add_pod(Exception('Oops!'))
+
+
+def _assert_pod_add_invoked_successfully(pod_file_json):
     pod_file_path = "some/path/to/pod.json"
     resource_reader = {pod_file_path: pod_file_json}.__getitem__
     marathon_client = create_autospec(marathon.Client)
@@ -19,3 +26,17 @@ def _assert_add_invoked_successfully(pod_file_json):
 
     assert returncode == 0
     marathon_client.add_pod.assert_called_with(pod_file_json)
+
+
+def _assert_pod_add_propagates_exceptions_from_add_pod(exception):
+    def resource_reader(path):
+        return {"some": "json"}
+
+    marathon_client = create_autospec(marathon.Client)
+    marathon_client.add_pod.side_effect = exception
+
+    subcmd = main.MarathonSubcommand(resource_reader, lambda: marathon_client)
+    with pytest.raises(exception.__class__) as exception_info:
+        subcmd.pod_add('does/not/matter')
+
+    assert exception_info.value == exception
