3b3eff70cae37c5f47a78ece64c8aa80742e2904
tamarrow <tamar@mesosphere.io>
2016-11-03 16:08:24 -0700

    http: don't retry requests if response is 401 (#824)
    We were retrying requests with 401 so that we can use the response
    headers to determine type of authentication. Instead, always send
    request with auth if we already have credentials (from
    core.dcos_acs_token).
    
    This also fixes multi-part requests that currently break if we have to
    retry the request (since the stream gets opened by the initial request).

--- cli/dcoscli/auth/main.py
+++ cli/dcoscli/auth/main.py
@@ -84,8 +84,9 @@ def _login():
 
     # hit protected endpoint which will prompt for auth if cluster has auth
     try:
-        url = urllib.parse.urljoin(dcos_url, 'exhibitor/')
-        http.get(url)
+        endpoint = '/pkgpanda/active.buildinfo.full.json'
+        url = urllib.parse.urljoin(dcos_url, endpoint)
+        http.request_with_auth('HEAD', url)
     # if the user is authenticated, they have effectively "logged in" even if
     # they are not authorized for this endpoint
     except DCOSAuthorizationException:
