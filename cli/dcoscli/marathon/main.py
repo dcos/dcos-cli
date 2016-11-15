92a21807f89c4e0c419a33573873c9cebe440e46
Johannes Unterstein <junterstein@mesosphere.io>
2016-11-15 15:47:16 +0100

    Renamed cli commands

--- cli/dcoscli/marathon/main.py
+++ cli/dcoscli/marathon/main.py
@@ -196,8 +196,13 @@ def _cmds():
             function=subcommand.queued_apps_list),
 
         cmds.Command(
-            hierarchy=['marathon', 'launchqueue', 'show'],
-            arg_keys=['<app-id>', '--details', '--json'],
+            hierarchy=['marathon', 'launchqueue', 'summary'],
+            arg_keys=['<app-id>', '--json'],
+            function=subcommand.queued_app_summary),
+
+        cmds.Command(
+            hierarchy=['marathon', 'launchqueue', 'details'],
+            arg_keys=['<app-id>', '--json'],
             function=subcommand.queued_app_details),
 
         cmds.Command(
@@ -990,12 +995,10 @@ class MarathonSubcommand(object):
                                tables.queued_apps_table, json_)
         return 0
 
-    def queued_app_details(self, app_id, details, json_):
+    def queued_app_details(self, app_id, json_):
         """
         :param app_id: the Marathon ID to display details
         :type app_id: string
-        :param details: output details if True
-        :type details: bool
         :param json_: output json if True
         :type json_: bool
         :returns: process return code
@@ -1006,14 +1009,30 @@ class MarathonSubcommand(object):
         queued_app = client.get_queued_app(app_id)
 
         if queued_app:
-            emitting.publish_table(emitter, queued_app,
-                                   tables.queued_app_table, json_)
+            emitting.publish_table(
+                emitter, queued_app,
+                tables.queued_app_details_table, json_)
 
-            if details:
-                emitter.publish('')
-                emitting.publish_table(
-                    emitter, queued_app,
-                    tables.queued_app_details_table, json_)
+
+        return 0
+
+    def queued_app_summary(self, app_id, json_):
+        """
+        :param app_id: the Marathon ID to display details
+        :type app_id: string
+        :param json_: output json if True
+        :type json_: bool
+        :returns: process return code
+        :rtype: int
+        """
+
+        client = self._create_marathon_client()
+        queued_app = client.get_queued_app(app_id)
+
+        if queued_app:
+            emitting.publish_table(
+                emitter, queued_app,
+                tables.queued_app_table, json_)
 
         return 0
 
