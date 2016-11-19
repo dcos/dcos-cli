f7e288ac29a955bb9e58ba14bbbf3f68d0f270af
Ken Sipe <kensipe@gmail.com>
2016-11-19 07:32:11 -0600

    passing pep8

--- cli/tests/integrations/test_marathon.py
+++ cli/tests/integrations/test_marathon.py
@@ -193,7 +193,9 @@ def test_show_bad_app_version():
         assert returncode == 1
         assert stdout == b''
         assert stderr.startswith(b'Error while fetching')
-        pattern = b"""{"message":"Invalid format: \\"20:39:32.972Z\\" is malformed at \\":39:32.972Z\\""}\n"""
+        pattern = (b"""{"message":"Invalid format: """
+                   b"""\\"20:39:32.972Z\\" is malformed"""
+                   b""" at \\":39:32.972Z\\""}\n""")
         assert stderr.endswith(pattern)
 
 
