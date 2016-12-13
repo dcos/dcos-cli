a3058502663e36f8e0ac558f011af0c28cc290c7
Kevin Klues <klueska@gmail.com>
2016-12-12 17:03:16 -0800

    Added environment variable to conditionally skip 'dcos task exec' tests.

--- cli/tests/integrations/test_task.py
+++ cli/tests/integrations/test_task.py
@@ -316,7 +316,8 @@ def test_ls_completed():
     assert stderr == b''
 
 
-@pytest.mark.skip(reason="Only available in DC/OS 1.9+")
+@pytest.mark.skipif('DCOS_DEBUGGING_ENABLED' not in os.environ,
+                    reason="Requires Agent Dbugging APIs")
 def test_exec_non_interactive():
     with open('tests/data/tasks/lorem-ipsum.txt') as text:
         content = text.read()
@@ -329,7 +330,8 @@ def test_exec_non_interactive():
             stdout=bytes(content, 'UTF-8'))
 
 
-@pytest.mark.skip(reason="Only available in DC/OS 1.9+")
+@pytest.mark.skipif('DCOS_DEBUGGING_ENABLED' not in os.environ,
+                    reason="Requires Agent Dbugging APIs")
 def test_exec_interactive():
     with open('tests/data/tasks/lorem-ipsum.txt') as text:
         content = bytes(text.read(), 'UTF-8')
