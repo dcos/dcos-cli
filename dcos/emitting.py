c2d59ff8a688ab64ecffeeecd7ed1e73867b80a7
Maksym Naboka <maksym.naboka@gmail.com>
2017-01-18 17:46:57 -0800

    emitting: add flush stdout after print (#877)

--- dcos/emitting.py
+++ dcos/emitting.py
@@ -155,6 +155,7 @@ def _page(output, pager_command=None):
 
     if not sys.stdout.isatty() or util.is_windows_platform():
         print(output)
+        sys.stdout.flush()
         return
 
     num_lines = output.count('\n')
