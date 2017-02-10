cfa6a7ede9fec6a2a5b3c89f86cd0f08f4f9589c
Marc Abramowitz <abramowi@adobe.com>
2017-02-10 11:56:15 -0800

    package.install => package/install

--- tests/test_packagemanager.py
+++ tests/test_packagemanager.py
@@ -76,6 +76,6 @@ def test_install(post_fn):
     post_fn.assert_called_with(
         'http://testserver/package/install',
         data=None,
-        headers=pkg_mgr.cosmos._get_header('package.install', 'v2'),
+        headers=pkg_mgr.cosmos._get_header('package/install', 'v2'),
         json={'packageName': pkg.name(), 'packageVersion': pkg.version()},
     )
