ca09a8e73eeb403313677d044e129ecdbeda246d
Philip Norman <philip@mesosphere.io>
2017-02-16 13:24:14 -0800

    Extract metrics-fetch code to a separate function

--- cli/dcoscli/metrics.py
+++ cli/dcoscli/metrics.py
@@ -9,24 +9,36 @@ logger = util.get_logger(__name__)
 emitter = emitting.FlatEmitter()
 
 
+def _fetch_node_metrics(url):
+    """Retrieve the output from`dcos-metrics`' `node` endpoint.
+
+    :param url: `dcos-metrics` `node` endpoint
+    :type url: str
+    """
+    with contextlib.closing(http.get(url)) as r:
+
+        # No need to guard against 204; metrics should never be empty
+        if r.status_code != 200:
+            raise DCOSException(
+                'Error getting metrics. Url: {};'
+                'response code: {}'.format(url, r.status_code))
+
+        return r.json()
+
+
 def print_node_metrics_table(url):
-    """Retrieve, parse, and pretty-print the output from `dcos-metrics`' `node`
+    """Retrieve and pretty-print the output from `dcos-metrics`' `node`
     endpoint.
 
     :param url: `dcos-metrics` `node` endpoint
     :type url: str
     """
+    node_json = _fetch_node_metrics(url)
+    datapoints = node_json.get('datapoints', [])
 
-    with contextlib.closing(http.get(url)) as r:
-
-        if r.status_code != 200:
-            raise DCOSException(
-                'Error getting logs. Url: {};'
-                'response code: {}'.format(url, r.status_code))
-
-        fields = OrderedDict([
-            ('NAME', lambda a: a['name']),
-            ('VALUE', lambda a: a['value'])
-        ])
-        table = tables.table(fields, r.json().get('datapoints', []))
-        return emitter.publish(table)
+    fields = OrderedDict([
+        ('NAME', lambda a: a['name']),
+        ('VALUE', lambda a: a['value'])
+    ])
+    table = tables.table(fields, datapoints)
+    return emitter.publish(table)
