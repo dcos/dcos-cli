0ab6245860cfe64be15fc1b7dda94fb29ee25597
Marc Abramowitz <abramowi@adobe.com>
2017-03-14 16:02:51 -0700

    split('\n') => splitlines()
    because `splitlines` is smarter about dealing with different line
    endings.
    
    It will be interesting to see if this test still passes or if it fails,
    like the one in #922.

--- cli/tests/integrations/test_node.py
+++ cli/tests/integrations/test_node.py
@@ -42,7 +42,7 @@ def test_node_table():
 
     assert returncode == 0
     assert stderr == b''
-    assert len(stdout.decode('utf-8').split('\n')) > 2
+    assert len(stdout.decode('utf-8').splitlines()) > 2
 
 
 def test_node_log_empty():
