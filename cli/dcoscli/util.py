b0b0a25d2d0ed5f611f11ebaa3a0c16dcb669730
Marc Abramowitz <abramowi@adobe.com>
2017-03-15 11:00:38 -0700

    Add `dcos config edit` command
    which opens tempfile copy of config file with $EDITOR and only updates
    the real config file if validation succeeds.
    
    Happy path, when the edits result in a valid config file:
    
    ```
    $ dcos config edit
    Executing: vim /var/folders/3b/sf0lrb1d69v8gfr8gvzxyy0h0000gn/T/tmp_rfx8308
    Updated file: /Users/abramowi/.dcos/dcos.toml
    ```
    
    Sad path, when user used some unknown parameter:
    
    ```
    $ dcos config edit
    Executing: vim /var/folders/3b/sf0lrb1d69v8gfr8gvzxyy0h0000gn/T/tmpzi8i4xpb
    Error: Additional properties are not allowed ('some_unknown_param' was unexpected)
    Path: core
    Value: {"some_unknown_param": "blah", ...}
    File /var/folders/3b/sf0lrb1d69v8gfr8gvzxyy0h0000gn/T/tmpzi8i4xpb failed validation check. Not updating /Users/abramowi/.dcos/dcos.toml.
    ```

--- cli/dcoscli/util.py
+++ cli/dcoscli/util.py
@@ -1,8 +1,11 @@
 from functools import wraps
+import os
+from shutil import copyfile
+from tempfile import NamedTemporaryFile
 
 import docopt
 
-from dcos import emitting
+from dcos import emitting, subprocess
 
 emitter = emitting.FlatEmitter()
 
@@ -26,3 +29,23 @@ def decorate_docopt_usage(func):
             return 1
         return result
     return wrapper
+
+
+def edit_file(filename, validate=None):
+    with NamedTemporaryFile() as temp_file_obj:
+        copyfile(filename, temp_file_obj.name)
+
+        editor = os.environ.get('EDITOR', 'vi')
+        cmd = '%s %s' % (editor, temp_file_obj.name)
+        emitter.publish('Executing: %s' % cmd)
+        subprocess.Subproc().call(cmd, shell=True)
+
+        if validate:
+            if not validate(temp_file_obj):
+                emitter.publish(
+                    'File %s failed validation check. Not updating %s.'
+                    % (temp_file_obj.name, filename))
+                return False
+
+        copyfile(temp_file_obj.name, filename)
+        emitter.publish('Updated file: %s' % filename)
