1827fe0bfd1e23fa07707d00d84596d4097e6dcf
tamarrow <tamar@mesosphere.io>
2017-04-13 17:13:38 -0700

    cluster: add tests (#972)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,13 @@
+import os
+
 import pytest
 
-from dcos import config
+from mock import patch
+
+from test_util import add_cluster_dir, create_global_config, env
+
+from dcos import config, constants, util
+from dcos.errors import DCOSException
 
 
 @pytest.fixture
@@ -110,3 +117,80 @@ def _conf():
             'repo_uri': 'git://localhost/mesosphere/package-repo.git'
         }
     }
+
+
+def test_uses_deprecated_config():
+    with env(), util.tempdir() as tempdir:
+        os.environ.pop('DCOS_CONFIG', None)
+        os.environ[constants.DCOS_DIR_ENV] = tempdir
+        assert config.get_config_dir_path() == tempdir
+
+        # create old global config toml
+        global_toml = create_global_config(tempdir)
+        assert config.get_global_config_path() == global_toml
+        assert config.uses_deprecated_config() is True
+
+        # create clusters subdir
+        _create_clusters_dir(tempdir)
+        assert config.uses_deprecated_config() is False
+
+
+def test_get_attached_cluster_path():
+    with env(), util.tempdir() as tempdir:
+        os.environ[constants.DCOS_DIR_ENV] = tempdir
+
+        # no clusters dir
+        assert config.get_attached_cluster_path() is None
+
+        # clusters dir, no clusters
+        _create_clusters_dir(tempdir)
+        assert config.get_attached_cluster_path() is None
+
+        # 1 cluster, not attached
+        cluster_id = "fake-cluster"
+        cluster_path = add_cluster_dir(cluster_id, tempdir)
+        assert config.get_attached_cluster_path() == cluster_path
+        attached_path = os.path.join(
+            cluster_path, constants.DCOS_CLUSTER_ATTACHED_FILE)
+        assert os.path.exists(attached_path)
+
+        # attached cluster
+        assert config.get_attached_cluster_path() == cluster_path
+
+
+@patch('dcos.config.load_from_path')
+def test_get_config(load_path_mock):
+    with env(), util.tempdir() as tempdir:
+        os.environ.pop('DCOS_CONFIG', None)
+        os.environ[constants.DCOS_DIR_ENV] = tempdir
+
+        # no config file of any type
+        with pytest.raises(DCOSException) as e:
+            config.get_config()
+
+        msg = ("No cluster is attached. "
+               "Please run `dcos cluster attach <cluster-name>`")
+        assert str(e.value) == msg
+        load_path_mock.assert_not_called()
+
+        # create old global config toml
+        global_toml = create_global_config(tempdir)
+        config.get_config()
+        load_path_mock.assert_called_once_with(global_toml, False)
+
+        # clusters dir, no clusters
+        _create_clusters_dir(tempdir)
+        with pytest.raises(DCOSException) as e:
+            config.get_config()
+        assert str(e.value) == msg
+
+        cluster_id = "fake-cluster"
+        cluster_path = add_cluster_dir(cluster_id, tempdir)
+        cluster_toml = os.path.join(cluster_path, "dcos.toml")
+        config.get_config(True)
+        load_path_mock.assert_any_call(cluster_toml, True)
+
+
+def _create_clusters_dir(dcos_dir):
+    clusters_dir = os.path.join(dcos_dir, constants.DCOS_CLUSTERS_SUBDIR)
+    util.ensure_dir_exists(clusters_dir)
