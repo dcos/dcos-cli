dcos/dcos-cli

commit 8971ccdb081f5b902387d72185fadb2cc6ed1fbd

task: add `dcos task metrics` (#923)

cli/dcoscli/task/main.py
@@ -1,6 +1,7 @@
 import os
 import posixpath
 import sys
+from functools import partial
 
 import docopt
 import six
@@ -9,9 +10,9 @@ import dcoscli
 from dcos import cmds, config, emitting, mesos, util
 from dcos.errors import DCOSException, DCOSHTTPException, DefaultError
 from dcoscli import log, tables
+from dcoscli import metrics
 from dcoscli.subcommand import default_command_info, default_doc
 from dcoscli.util import decorate_docopt_usage
-
 
 logger = util.get_logger(__name__)
 emitter = emitting.FlatEmitter()
@@ -138,6 +139,16 @@ def _cmds():
             function=_exec),
 
         cmds.Command(
+            hierarchy=['task', 'metrics', 'details'],
+            arg_keys=['<task-id>', '--json'],
+            function=partial(_metrics, False)),
+
+        cmds.Command(
+            hierarchy=['task', 'metrics', 'summary'],
+            arg_keys=['<task-id>', '--json'],
+            function=partial(_metrics, True)),
+
+        cmds.Command(
             hierarchy=['task'],
             arg_keys=['<task>', '--all', '--completed', '--json'],
             function=_task),
@@ -171,8 +182,8 @@ def _task(task, all_, completed, json_):
     """
 
     tasks = sorted(mesos.get_master().tasks(
-                   fltr=task, completed=completed, all_=all_),
-                   key=lambda t: t['name'])
+        fltr=task, completed=completed, all_=all_),
+        key=lambda t: t['name'])
 
     if json_:
         emitter.publish([t.dict() for t in tasks])
@@ -550,3 +561,38 @@ def _load_slaves_state(slaves):
                 DefaultError('Error accessing slave: {0}'.format(e)))
 
     return reachable_slaves
+
+
+def _metrics(summary, task_id, json_):
+    """
+    Get metrics from the specified task.
+
+    :param summary: summarise output if true, output all if false
+    :type summary: bool
+    :param task_id: mesos task id
+    :type task_id: str
+    :param json: print raw JSON
+    :type json: bool
+    :return: Process status
+    :rtype: int
+    """
+
+    master = mesos.get_master()
+    task = master.task(task_id)
+    if 'slave_id' not in task:
+        raise DCOSException(
+            'Error finding agent associated with task: {}'.format(task_id))
+
+    slave_id = task['slave_id']
+    container_id = master.get_container_id(task_id)
+
+    endpoint = '/system/v1/agent/{}/metrics/v0/containers/{}'.format(
+        slave_id, container_id
+    )
+    dcos_url = config.get_config_val('core.dcos_url').rstrip('/')
+    if not dcos_url:
+        raise config.missing_config_exception(['core.dcos_url'])
+
+    url = dcos_url + endpoint
+    app_url = url + '/app'
+    return metrics.print_task_metrics(url, app_url, summary, json_)

dcos/mesos.py
@@ -496,7 +496,7 @@ class Master(object):
         def _get_container_id(container_status):
             if 'container_id' in container_status:
                 if 'value' in container_status['container_id']:
-                    return container_status['container_id']
+                    return container_status['container_id']['value']
 
             raise DCOSException(
                 "No container found for the specified task."
@@ -802,6 +802,17 @@ class Task(object):
         """
 
         return self._task[name]
+
+    def __contains__(self, name):
+        """Supprt the `attr in task` syntax
+
+        :param name: attribute to test
+        :type name: str
+        :returns: True if attribute is present in the underlying dict
+        :rtype: bool
+        """
+
+        return name in self._task
 
 
 class MesosFile(object):

cli/tests/integrations/test_task.py
@@ -11,7 +11,7 @@ import pytest
 import dcos.util as util
 from dcos.util import create_schema
 
-from .helpers.common import assert_command, assert_lines, exec_command
+from .helpers.common import (assert_command, assert_lines, exec_command)
 from .helpers.marathon import (add_app, app, pod, remove_app,
                                watch_all_deployments)
 from ..fixtures.task import task_fixture
@@ -75,7 +75,7 @@ def test_task():
 
 
 def test_task_table():
-    assert_lines(['dcos', 'task'], NUM_TASKS+1)
+    assert_lines(['dcos', 'task'], NUM_TASKS + 1)
 
 
 def test_task_completed():
@@ -121,7 +121,6 @@ def test_log_single_file():
 def test_log_pod_task():
     good_pod_file = 'tests/data/marathon/pods/good.json'
     with pod(good_pod_file, 'good-pod'):
-
         returncode, stdout, stderr = exec_command(
             ['dcos', 'task', 'log', 'good-container', 'stderr'])
 

cli/tests/unit/test_task.py
@@ -4,7 +4,7 @@ from mock import MagicMock, patch
 from dcos import mesos
 from dcos.errors import DCOSException
 from dcoscli.log import log_files
-from dcoscli.task.main import _dcos_log, main
+from dcoscli.task.main import _dcos_log, _metrics, main
 
 from .common import assert_mock
 
@@ -142,3 +142,52 @@ def test_dcos_log_stream(mocked_get_config_val, mocked_http_get,
         'http://127.0.0.1/system/v1/agent/slave-123/logs/v1/'
         'stream/framework/framework-123/executor/id-123/container/'
         'child-123?skip_prev=10&filter=STREAM:STDERR')
+
+
+@patch('dcos.http.get')
+@patch('dcos.mesos.get_master')
+@patch('dcos.config.get_config_val')
+def test_dcos_task_metrics_agent_details(mocked_get_config_val,
+                                         mocked_get_master,
+                                         mocked_http_get):
+    mocked_get_config_val.return_value = 'http://127.0.0.1'
+
+    mock_http_response = MagicMock()
+    mock_http_response.status_code = 200
+    mocked_http_get.return_value = mock_http_response
+
+    mock_master = MagicMock()
+    mock_master.task = lambda _: {'slave_id': 'slave_id'}
+    mock_master.get_container_id = lambda _: 'container_id'
+    mocked_get_master.return_value = mock_master
+
+    _metrics(True, 'task_id', False)
+
+    # Metrics should query both endpoints
+    mocked_http_get.assert_any_call('http://127.0.0.1/system/v1/agent/'
+                                    'slave_id/metrics/v0/containers/'
+                                    'container_id/app')
+    mocked_http_get.assert_any_call('http://127.0.0.1/system/v1/agent/'
+                                    'slave_id/metrics/v0/containers/'
+                                    'container_id')
+
+
+@patch('dcos.http.get')
+@patch('dcos.mesos.get_master')
+@patch('dcos.config.get_config_val')
+def test_dcos_task_metrics_agent_missing_slave(mocked_get_config_val,
+                                               mocked_get_master,
+                                               mocked_http_get):
+    mocked_get_config_val.return_value = 'http://127.0.0.1'
+
+    mock_http_response = MagicMock()
+    mock_http_response.status_code = 200
+    mocked_http_get.return_value = mock_http_response
+
+    mock_master = MagicMock()
+    mock_master.task = lambda _: {}
+    mocked_get_master.return_value = mock_master
+
+    # Should a task not have a slave ID, expect an error
+    with pytest.raises(DCOSException):
+        _metrics(True, 'task_id', False)

cli/dcoscli/metrics.py
@@ -13,10 +13,16 @@ def _gib(n):
     return n * pow(2, -30)
 
 
-def _fetch_node_metrics(url):
-    """Retrieve the metrics data from `dcos-metrics`' `node` endpoint.
+def _percentage(dividend, divisor):
+    if divisor > 0:
+        return dividend / divisor * 100
+    return 0
 
-    :param url: `dcos-metrics` `node` endpoint
+
+def _fetch_metrics_datapoints(url):
+    """Retrieve the metrics data from any `dcos-metrics` endpoint.
+
+    :param url: `dcos-metrics` endpoint
     :type url: str
     :returns: List of metrics datapoints
     :rtype: [dict]
@@ -57,6 +63,25 @@ def _get_datapoint(datapoints, name, tags=None):
                 return datapoint
 
 
+def _get_datapoint_value(datapoints, name, tags=None):
+    """Safely return only the value from a datapoint, defaulting to 0.
+
+    :param datapoints: a list of datapoints
+    :type datapoints: [dict]
+    :param name: the name of the required datapoint
+    :type name: str
+    :param tags: required tags by key and value
+    :type tags: dict
+    :return: a matching datapoint
+    :rtype: float
+    """
+    datapoint = _get_datapoint(datapoints, name, tags)
+    if datapoint is not None:
+        return datapoint.get('value', 0.0)
+
+    return 0.0
+
+
 def _node_summary_json(datapoints):
     """Filters datapoints down to CPU, memory and root disk space fields.
 
@@ -82,24 +107,66 @@ def _node_summary_data(datapoints):
     :rtype: dict
     """
 
-    def _percentage(dividend, divisor):
-        if divisor > 0:
-            return dividend / divisor * 100
-        return 0
+    cpu_used = _get_datapoint_value(datapoints, 'load.1min')
+    cpu_used_pc = _get_datapoint_value(datapoints, 'cpu.total')
 
-    cpu_used = _get_datapoint(datapoints, 'load.1min')['value']
-    cpu_used_pc = _get_datapoint(datapoints, 'cpu.total')['value']
-
-    mem_total = _get_datapoint(datapoints, 'memory.total')['value']
-    mem_free = _get_datapoint(datapoints, 'memory.free')['value']
+    mem_total = _get_datapoint_value(datapoints, 'memory.total')
+    mem_free = _get_datapoint_value(datapoints, 'memory.free')
     mem_used = mem_total - mem_free
     mem_used_pc = _percentage(mem_used, mem_total)
 
-    disk_total = _get_datapoint(
-        datapoints, 'filesystem.capacity.total', {'path': '/'})['value']
-    disk_free = _get_datapoint(
-        datapoints, 'filesystem.capacity.used', {'path': '/'})['value']
+    disk_total = _get_datapoint_value(
+        datapoints, 'filesystem.capacity.total', {'path': '/'})
+    disk_free = _get_datapoint_value(
+        datapoints, 'filesystem.capacity.used', {'path': '/'})
     disk_used = disk_total - disk_free
+    disk_used_pc = _percentage(disk_used, disk_total)
+
+    return {
+        'cpu': '{:0.2f} ({:0.2f}%)'.format(cpu_used, cpu_used_pc),
+        'mem': '{:0.2f}GiB ({:0.2f}%)'.format(_gib(mem_used), mem_used_pc),
+        'disk': '{:0.2f}GiB ({:0.2f}%)'.format(_gib(disk_used), disk_used_pc)
+    }
+
+
+def _task_summary_json(datapoints):
+    """Filters datapoints down to CPU, memory and disk space fields.
+
+    :param datapoints: a list of datapoints
+    :type datapoints: [dict]
+    :return: JSON data
+    :rtype: str
+    """
+    summary_datapoints = [
+        _get_datapoint(datapoints, 'cpus.user.time'),
+        _get_datapoint(datapoints, 'mem.total'),
+        _get_datapoint(datapoints, 'disk.used')
+    ]
+    return json.dumps(summary_datapoints)
+
+
+def _task_summary_data(datapoints):
+    """Extracts CPU, memory and root disk space fields from task datapoints.
+
+    :param datapoints: a list of raw datapoints
+    :type datapoints: [dict]
+    :return: a dictionary of summary fields
+    :rtype: dict
+    """
+
+    cpu_user = _get_datapoint_value(datapoints, 'cpus.user.time')
+    cpu_system = _get_datapoint_value(datapoints, 'cpus.system.time')
+    cpu_throttled = _get_datapoint_value(datapoints, 'cpus.throttled.time')
+    cpu_used = cpu_user + cpu_system
+    cpu_total = cpu_used + cpu_throttled
+    cpu_used_pc = _percentage(cpu_used, cpu_total)
+
+    mem_total = _get_datapoint_value(datapoints, 'mem.limit')
+    mem_used = _get_datapoint_value(datapoints, 'mem.total')
+    mem_used_pc = _percentage(mem_used, mem_total)
+
+    disk_total = _get_datapoint_value(datapoints, 'disk.used')
+    disk_used = _get_datapoint_value(datapoints, 'disk.limit')
     disk_used_pc = _percentage(disk_used, disk_total)
 
     return {
@@ -132,6 +199,8 @@ def _format_datapoints(datapoints):
             return '{:0.2f}GiB'.format(_gib(v))
         if u == 'percent':
             return '{:0.2f}%'.format(v)
+        if type(v) == float:
+            return '{:0.2f}'.format(v)
         return v
 
     formatted_datapoints = []
@@ -159,7 +228,7 @@ def print_node_metrics(url, summary, json_):
     :rtype: int
     """
 
-    datapoints = _fetch_node_metrics(url)
+    datapoints = _fetch_metrics_datapoints(url)
 
     if summary:
         if json_:
@@ -169,5 +238,37 @@ def print_node_metrics(url, summary, json_):
         if json_:
             return emitter.publish(datapoints)
         table = tables.metrics_details_table(_format_datapoints(datapoints))
+
+    return emitter.publish(table)
+
+
+def print_task_metrics(url, app_url, summary, json_):
+    """Retrieve and pretty-print fields from the `dcos-metrics`' `containers/id`
+    endpoint and `containers/id/app` endpoint.
+
+    :param url: `dcos-metrics` `containers/id` endpoint
+    :type url: str
+    :param app_url: `dcos-metrics` `containers/id/app` endpoint
+    :type app_url: str
+    :param json_: print json list if true
+    :type json_: bool
+    :param summary: print summary if true, or all fields if false
+    :type summary: bool
+    :return: Process status
+    :rtype: int
+    """
+
+    datapoints = _fetch_metrics_datapoints(url) + _fetch_metrics_datapoints(
+        app_url)
+
+    if summary:
+        if json_:
+            return emitter.publish(_task_summary_json(datapoints))
+        table = tables.metrics_summary_table(_task_summary_data(datapoints))
+    else:
+        if json_:
+            return emitter.publish(datapoints)
+        table = tables.metrics_details_table(_format_datapoints(datapoints),
+                                             False)
 
     return emitter.publish(table)

cli/dcoscli/tables.py
@@ -934,6 +934,7 @@ def metrics_summary_table(data):
 
     :param data: A dictionary of formatted summary values.
     :type data: dict
+    :rtype: PrettyTable
     """
     fields = OrderedDict([
         ('CPU', lambda d: d['cpu']),
@@ -950,23 +951,29 @@ def metrics_summary_table(data):
     return metrics_table
 
 
-def metrics_details_table(datapoints):
+def metrics_details_table(datapoints, show_tags=True):
     """Prints a table of all passed metrics
 
     :param datapoints: A raw list of datapoints
     :type datapoints: [dict]
+    :param show_tags: Show column for tags, unless False
+    :type show_tags: bool
+    :rtype: PrettyTable
     """
 
-    fields = OrderedDict([
+    field_defs = [
         ('NAME', lambda d: d['name']),
         ('VALUE', lambda d: d['value']),
-        ('TAGS', lambda d: d['tags'])
-    ])
+    ]
+    if show_tags:
+        field_defs.append(('TAGS', lambda d: d['tags']))
+
+    fields = OrderedDict(field_defs)
 
     metrics_table = table(fields, datapoints)
-    metrics_table.align['NAME'] = 'l'
-    metrics_table.align['VALUE'] = 'l'
-    metrics_table.align['TAGS'] = 'l'
+    for (k, v) in field_defs:
+        metrics_table.align[k] = 'l'
+
     return metrics_table
 
 

cli/tests/fixtures/metrics.py
@@ -46,3 +46,22 @@ def agent_metrics_node_summary_fixture():
         'mem': '2.49GiB (16.98%)',
         'disk': '1.65GiB (30.30%)'
     }
+
+
+def agent_metrics_task_details_fixture():
+    """Agent metrics /container fixture
+
+    :rtype: [dict]"""
+
+    tags = "executor_id: abc-123, source: consume-cpui.d3df66d3-08dc-11e7-8d53"
+
+    return [
+        {"name": "cpus.user.time", "tags": tags, "value": 4902.86},
+        {"name": "cpus.system.time", "tags": tags, "value": 8749.93},
+        {"name": "cpus.limit", "tags": tags, "value": 0.6},
+        {"name": "cpus.throttled.time", "tags": tags, "value": 100.01},
+        {"name": "mem.total", "tags": tags, "value": "0.01GiB"},
+        {"name": "mem.limit", "tags": tags, "value": "0.16GiB"},
+        {"name": "disk.limit", "tags": tags, "value": "0.00GiB"},
+        {"name": "disk.used", "tags": tags, "value": "0.00GiB"},
+    ]

cli/tests/unit/test_tables.py
@@ -15,7 +15,8 @@ from ..fixtures.marathon import (app_fixture, app_task_fixture,
                                  pod_list_without_instances_fixture,
                                  pod_list_without_spec_version_fixture)
 from ..fixtures.metrics import (agent_metrics_node_details_fixture,
-                                agent_metrics_node_summary_fixture)
+                                agent_metrics_node_summary_fixture,
+                                agent_metrics_task_details_fixture)
 from ..fixtures.node import slave_fixture
 from ..fixtures.package import package_fixture, search_result_fixture
 from ..fixtures.service import framework_fixture
@@ -136,7 +137,15 @@ def test_metrics_summary_table():
 def test_metrics_details_table():
     _test_table(tables.metrics_details_table,
                 agent_metrics_node_details_fixture(),
-                'tests/unit/data/metrics_details.txt')
+                'tests/unit/data/metrics_node_details.txt')
+
+
+def test_metrics_details_no_tags_table():
+    # Convenience wrapper to pass a second param
+    def _task_details_fn(datapoints):
+        return tables.metrics_details_table(datapoints, False)
+    _test_table(_task_details_fn, agent_metrics_task_details_fixture(),
+                'tests/unit/data/metrics_task_details.txt')
 
 
 def _test_table(table_fn, fixture_fn, path):

commit 6ebbcd037d6e61b731eaf48b8d4f560341a8bf3c

Undo reverted changes that cause `dcos task exec` to break.
This commit:
https://github.com/dcos/dcos-cli/commit/8971ccdb081f5b902387d72185fadb2cc6ed1fbd

Inadvertently undid changes originally introduced in this commit:
https://github.com/dcos/dcos-cli/commit/f33a45ee94377e2b4654d9d69812320e4b2b823b

Unfortunately, the integration tests didn't catch this because all
`task exec` integration tests were hidden behind an environment variable:

    @pytest.mark.skipif('DCOS_DEBUGGING_ENABLED' not in os.environ,
                        reason="Requires Agent Debugging APIs")

This commit reintroduces the changes that were inadvertently undone and
enables the `task exec` integration tests by default so that something
like this doesn't occur in the future.

cli/dcoscli/task/main.py
@@ -584,7 +584,7 @@ def _metrics(summary, task_id, json_):
             'Error finding agent associated with task: {}'.format(task_id))
 
     slave_id = task['slave_id']
-    container_id = master.get_container_id(task_id)
+    container_id = master.get_container_id(task_id)["value"]
 
     endpoint = '/system/v1/agent/{}/metrics/v0/containers/{}'.format(
         slave_id, container_id

dcos/mesos.py
@@ -496,7 +496,7 @@ class Master(object):
         def _get_container_id(container_status):
             if 'container_id' in container_status:
                 if 'value' in container_status['container_id']:
-                    return container_status['container_id']['value']
+                    return container_status['container_id']
 
             raise DCOSException(
                 "No container found for the specified task."

cli/tests/integrations/test_task.py
@@ -323,8 +323,6 @@ def test_ls_completed():
     assert stderr == b''
 
 
-@pytest.mark.skipif('DCOS_DEBUGGING_ENABLED' not in os.environ,
-                    reason="Requires Agent Debugging APIs")
 def test_exec_non_interactive():
     with open('tests/data/tasks/lorem-ipsum.txt') as text:
         content = text.read()
@@ -337,8 +335,6 @@ def test_exec_non_interactive():
             stdout=bytes(content, 'UTF-8'))
 
 
-@pytest.mark.skipif('DCOS_DEBUGGING_ENABLED' not in os.environ,
-                    reason="Requires Agent Debugging APIs")
 def test_exec_interactive():
     with open('tests/data/tasks/lorem-ipsum.txt') as text:
         content = bytes(text.read(), 'UTF-8')

cli/tests/unit/test_task.py
@@ -158,7 +158,10 @@ def test_dcos_task_metrics_agent_details(mocked_get_config_val,
 
     mock_master = MagicMock()
     mock_master.task = lambda _: {'slave_id': 'slave_id'}
-    mock_master.get_container_id = lambda _: 'container_id'
+    mock_master.get_container_id = lambda _: {
+        'parent': {},
+        'value': 'container_id'
+    }
     mocked_get_master.return_value = mock_master
 
     _metrics(True, 'task_id', False)