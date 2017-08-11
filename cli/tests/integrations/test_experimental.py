580414f778d31811b9b909dfca445a7ab14d2a19
Bilal Amarni <bilal.amarni@gmail.com>
2017-08-11 13:11:50 +0200

    Fix a typo in a test function name

--- cli/tests/integrations/test_experimental.py
+++ cli/tests/integrations/test_experimental.py
@@ -185,7 +185,7 @@ def test_package_build_where_build_definition_has_badly_formed_reference():
                                stderr=stderr)
 
 
-def test_package_add_argument_exclussion():
+def test_package_add_argument_exclusion():
     command = command_base + ['package', 'add',
                               '--dcos-package', runnable_package_path(1),
                               '--package-version', '3.0']
