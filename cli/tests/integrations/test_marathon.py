a6f3c9a29213ee343fc70e5d0312e171c64f0f5c
Adam Dangoor <adamdangoor@gmail.com>
2017-09-13 11:48:50 +0100

    Try fixing integration tests

--- cli/tests/integrations/test_marathon.py
+++ cli/tests/integrations/test_marathon.py
@@ -169,7 +169,7 @@ def test_show_bad_app_version():
         assert stderr.startswith(b'Error while fetching')
         pattern = (b"""{"message":"Invalid format: """
                    b"""\\"20:39:32.972Z\\" is malformed"""
-                   b""" at \\":39:32.972Z\\""}.\n""")
+                   b""" at \\":39:32.972Z\\""}".\n""")
         assert stderr.endswith(pattern)
 
 
@@ -262,7 +262,7 @@ def test_update_invalid_request():
     # TODO (tamar): this becomes 'Error: App '/{' does not exist\n"'
     # in Marathon 0.11.0
     assert stderr.startswith('Error on request')
-    assert stderr.endswith('HTTP 400: Bad Request\n')
+    assert stderr.endswith('HTTP 400: Bad Request.\n')
 
 
 def test_app_add_invalid_request():
@@ -564,7 +564,7 @@ def test_show_missing_task():
     assert returncode == 1
     assert stdout == b''
     assert stderr.startswith("Task '")
-    assert stderr.endswith("' does not exist\n")
+    assert stderr.endswith("' does not exist.\n")
 
 
 def test_show_task():
