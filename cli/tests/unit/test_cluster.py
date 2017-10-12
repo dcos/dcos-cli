775ab42355188d0541e57c1e1c66ecbb4ec51b9c
Bilal Amarni <bilal.amarni@gmail.com>
2017-10-12 12:44:16 +0200

    Don't use custom CA if HTTPS works out of the box
    If it works there is no need to download and use the custom CA.
    This usually happens when we're dealing with a load-balancer
    or when a custom CA has already been added to the system.
    
    https://jira.mesosphere.com/browse/DCOS-19040

---
+++ cli/tests/unit/test_cluster.py
@@ -0,0 +1,29 @@
+import requests
+from mock import create_autospec, patch
+
+from dcoscli.cluster.main import _needs_cluster_cert
+
+
+@patch('requests.get')
+def test_needs_cluster_cert_because_ssl_error(req_mock):
+    req_mock.side_effect = requests.exceptions.SSLError()
+
+    assert _needs_cluster_cert('https://example.com')
+
+
+@patch('requests.get')
+def test_does_not_need_cluster_cert_because_non_https(req_mock):
+    resp = create_autospec(requests.Response)
+    resp.status_code = 200
+    req_mock.return_value = resp
+
+    assert _needs_cluster_cert('http://example.com') is False
+
+
+@patch('requests.get')
+def test_does_not_need_cluster_cert_because_https_works(req_mock):
+    resp = create_autospec(requests.Response)
+    resp.status_code = 200
+    req_mock.return_value = resp
+
+    assert _needs_cluster_cert('https://example.com') is False
