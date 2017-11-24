484a40dbe5c91cbb69b50ecea38ffbb5da331c47
Bilal Amarni <bilal.amarni@gmail.com>
2017-11-24 18:51:16 +0100

    Add command options for `dcos cluster setup`
    This configures all option flags for the `cluster setup` subcommand.

--- dcoscli/dcos_cli/cli/cluster/setup.py
+++ dcoscli/dcos_cli/cli/cluster/setup.py
@@ -3,7 +3,52 @@
 import click
 
 
+# pylint: disable=too-many-arguments
+
 @click.command(name='setup')
-def cluster_setup():
+@click.option(
+    '--ca-certs',
+    type=click.Path(exists=True),
+    help='Specify the path to a CA bundle to verify requests against.'
+)
+@click.option(
+    '--insecure',
+    is_flag=True,
+    help='Allow requests to bypass SSL certificate verification (insecure).'
+)
+@click.option(
+    '--no-check',
+    is_flag=True,
+    help='Do not check CA certficate downloaded from cluster (insecure).'
+)
+@click.option(
+    '--password',
+    help='Specify password on the command line (insecure).'
+)
+@click.option(
+    '--password-env',
+    help='Specify an environment variable name that contains the password.'
+)
+@click.option(
+    '--password-file',
+    type=click.Path(exists=True),
+    help='Specify the path to a file that contains the password.'
+)
+@click.option(
+    '--private-key',
+    type=click.Path(exists=True),
+    help='Specify the path to a file that contains the private key.'
+)
+@click.option(
+    '--provider',
+    help='Specify the authentication provider to use for login.'
+)
+@click.option(
+    '--username',
+    help='Specify the username for login.'
+)
+def cluster_setup(ca_certs, insecure, no_check, password, password_env,
+                  password_file, private_key, provider, username):
     """Set up a cluster."""
+    # pylint: disable=unused-argument
     print('Setup a cluster.')
