6263db2685b69be64bc0fde3a3f8ea673761100c
Brian Lewis <keller@csh.rit.edu>
2018-03-27 16:21:41 +0200

    Add verbose auth type descriptions and change the table output to use the auth name rather than the type described in the provider object (in line with the Python CLI)

--- pkg/cmd/dcos_auth_listproviders.go
+++ pkg/cmd/dcos_auth_listproviders.go
@@ -2,11 +2,23 @@ package cmd
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/dcos/dcos-cli/pkg/httpclient"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	"os"
+)
+
+// These are the different auth types that DC/OS supports with the names that they'll be given from the providers
+// endpoint.
+const (
+	AuthTypeDCOSUidPassword = "dcos-uid-password"
+	AuthTypeDCOSUidServiceKey = "dcos-uid-servicekey"
+	AuthTypeDCOSUidPasswordLDAP = "dcos-uid-password-ldap"
+	AuthTypeSAMLSpInitiated = "saml-sp-initiated"
+	AuthTypeOIDCAuthCodeFlow = "oidc-authorization-code-flow"
+	AuthTypeOIDCImplicitFlow = "oidc-implicit-flow"
 )
 
 var jsonOutput bool
@@ -41,13 +53,20 @@ func listProviders(cmd *cobra.Command, args []string) {
 	} else {
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{"PROVIDER ID", "AUTHENTICATION TYPE"})
+		// turn off wrapping because it seems to wrap even if the column is set to be wide enough
+		table.SetAutoWrapText(false)
 		table.SetBorder(false)
 		table.SetRowSeparator(" ")
 		table.SetColumnSeparator(" ")
 		table.SetCenterSeparator(" ")
 
-		for _, v := range *providers {
-			table.Append([]string{v.AuthenticationType, v.Description})
+		for name, provider := range *providers {
+			desc, err := authTypeDescription(provider.AuthenticationType, provider)
+			if err != nil {
+				fmt.Printf("Unknown authentication type %s", provider.AuthenticationType)
+				os.Exit(1)
+			}
+			table.Append([]string{name, desc})
 		}
 		table.Render()
 	}
@@ -69,6 +88,24 @@ func getProviders() (*map[string]authProvider, error) {
 	}
 
 	return &resp, nil
+}
+
+func authTypeDescription(authType string, provider authProvider) (string, error) {
+	var desc string
+	if authType == AuthTypeDCOSUidPassword {
+		desc = "Log in using a standard DC/OS user account (username and password)"
+	} else if authType == AuthTypeDCOSUidServiceKey {
+		desc = "Log in using a DC/OS service user account (username and private key)"
+	} else if authType == AuthTypeDCOSUidPasswordLDAP {
+		desc = "Log in in using an LDAP user account (username and password)"
+	} else if authType == AuthTypeSAMLSpInitiated {
+		desc = fmt.Sprintf("Log in using SAML 2.0 (%s)", provider.Description)
+	} else if authType == AuthTypeOIDCAuthCodeFlow || authType == AuthTypeOIDCImplicitFlow {
+		desc = fmt.Sprintf("Log in using OpenID Connect(%s)", provider.Description)
+	} else {
+		return "", errors.New("unknown authentication type")
+	}
+	return desc, nil
 }
 
 type authProvider struct {
