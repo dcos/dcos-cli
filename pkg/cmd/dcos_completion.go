836fbe6401e5b928a94872545717fbafec335e76
Brian Lewis <keller@csh.rit.edu>
2018-05-08 15:48:38 +0200

    start on bash completion command

---
+++ pkg/cmd/dcos_completion.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/dcos/dcos-cli/pkg/cli"
+	"github.com/dcos/dcos-cli/pkg/subcommand"
+	"github.com/spf13/cobra"
+)
+
+func newSubCmdCompletion(ctx *cli.Context) subcommand.SubCommand {
+	sc := subcommand.NewInternalSubCommand(newCmdCompletion(ctx))
+	return sc
+}
+
+func newCmdCompletion(ctx *cli.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:       "completion",
+		ValidArgs: []string{"bash", "zsh"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	return cmd
+}
