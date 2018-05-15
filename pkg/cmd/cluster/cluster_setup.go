053fa738c0bfb6d22d10a5c41d56aa7ce777124c
Bilal Amarni <bilal.amarni@gmail.com>
2018-05-15 11:43:49 +0200

    Add `dcos cluster setup`
    It is similar to the current one in Python except that it doesn't rely
    on temporary folders. Instead the setup happens in memory and the config
    is flushed to the disk on success.

---
+++ pkg/cmd/cluster/cluster_setup.go
@@ -0,0 +1,30 @@
+package cluster
+
+import (
+	"strings"
+
+	"github.com/dcos/dcos-cli/api"
+	"github.com/dcos/dcos-cli/pkg/setup"
+
+	"github.com/spf13/cobra"
+)
+
+// newCmdClusterSetup configures the CLI with a given DC/OS cluster.
+func newCmdClusterSetup(ctx api.Context) *cobra.Command {
+	setupFlags := setup.NewFlags(ctx.Fs(), ctx.EnvLookup)
+	cmd := &cobra.Command{
+		Use:  "setup",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clusterURL := args[0]
+
+			// This supports passing a cluster URL without scheme, it then defaults to HTTPS.
+			if !strings.HasPrefix(clusterURL, "https://") && !strings.HasPrefix(clusterURL, "http://") {
+				clusterURL = "https://" + clusterURL
+			}
+			return ctx.Setup(setupFlags, clusterURL)
+		},
+	}
+	setupFlags.Register(cmd.Flags())
+	return cmd
+}
---
+++ pkg/setup/flags.go
@@ -0,0 +1,60 @@
+package setup
+
+import (
+	"github.com/dcos/dcos-cli/pkg/login"
+	"github.com/spf13/afero"
+	"github.com/spf13/pflag"
+)
+
+// Flags are command-line flags for a cluster setup.
+type Flags struct {
+	caBundle     []byte
+	caBundlePath string
+	noCheck      bool
+	insecure     bool
+	loginFlags   *login.Flags
+	fs           afero.Fs
+}
+
+// NewFlags creates flags for a cluster setup.
+func NewFlags(fs afero.Fs, envLookup func(key string) (string, bool)) *Flags {
+	return &Flags{
+		fs:         fs,
+		loginFlags: login.NewFlags(fs, envLookup),
+	}
+}
+
+// Register registers cluster setup flags.
+func (f *Flags) Register(flags *pflag.FlagSet) {
+	flags.BoolVar(
+		&f.insecure,
+		"insecure",
+		false,
+		"Allow requests to bypass TLS certificate verification (insecure).",
+	)
+	flags.BoolVar(
+		&f.noCheck,
+		"no-check",
+		false,
+		"Do not check CA certficate downloaded from cluster (insecure). Applies to Enterprise DC/OS only.",
+	)
+	flags.StringVar(
+		&f.caBundlePath,
+		"ca-certs",
+		"",
+		"Specify the path to a file with trusted CAs to verify requests against.",
+	)
+	f.loginFlags.Register(flags)
+}
+
+// Resolve resolves setup and login flags.
+func (f *Flags) Resolve() error {
+	if f.caBundlePath != "" {
+		caBundle, err := afero.ReadFile(f.fs, f.caBundlePath)
+		if err != nil {
+			return err
+		}
+		f.caBundle = caBundle
+	}
+	return f.loginFlags.Resolve()
+}
