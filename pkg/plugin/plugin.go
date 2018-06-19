adcf048b90a531285a004b07406c28a45185ae83
Brian Lewis <keller@csh.rit.edu>
2018-06-19 17:25:40 +0200

    remove fields and structs for non top level plugin commands. They aren't being used right now

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -33,36 +33,13 @@ type Executable struct {
 
 // Command is a command living within a plugin binary
 type Command struct {
-	Name        string      `yaml:"name"`
-	Description string      `yaml:"description"`
-	Flags       []*Flag     `yaml:"flags"`
-	Args        []*Argument `yaml:"args"`
-	Subcommands []*Command  `yaml:"subcommands"`
-}
-
-// Flag represents a flag option on a command
-type Flag struct {
 	Name        string `yaml:"name"`
-	Shorthand   string `yaml:"shorthand"`
 	Description string `yaml:"description"`
-	Type        string `yaml:"type"`
 }
 
 type source struct {
 	Type string `yaml:"zip"`
 	URL  string `yaml:"url"`
-}
-
-// Argument represents an argument of a subcommand
-type Argument struct {
-	Name        string `yaml:"name"`
-	Description string `yaml:"description"`
-	Type        string `yaml:"type"`
-	// Multiple indicates that this command takes an arbitrary number of arguments of this type. This means
-	// that it needs to be only on the last argument in the list.
-	// TODO: loading plugins should probably be handled in this package because multiple is something that
-	// should be checked on plugin load.
-	Mutliple bool `yaml:"multiple"`
 }
 
 // IntoCommands creates a list of cobra commands from the commands available in the plugin
@@ -84,6 +61,7 @@ func (p *Plugin) IntoCommands(ctx *cli.Context) []*cobra.Command {
 func (c *Command) IntoCommand(ctx *cli.Context, dir string, exe string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                c.Name,
+		Short:              c.Description,
 		DisableFlagParsing: true,
 		SilenceUsage:       true, // Silences usage information from the wrapper CLI on error
 		SilenceErrors:      true, // Silences error message if called binary returns an error exit code
@@ -108,12 +86,6 @@ func (c *Command) IntoCommand(ctx *cli.Context, dir string, exe string) *cobra.C
 			return nil
 		},
 	}
-
-	/*
-		for _, s := range c.Subcommands {
-			cmd.AddCommand(s.IntoCommand(exe))
-		}
-	*/
 
 	return cmd
 }
