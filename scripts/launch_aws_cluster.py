0f0e77ac0e295de85ce68e65380b5223d8eb2807
Bilal Amarni <bilal.amarni@gmail.com>
2018-06-26 18:27:07 +0200

    Backport Jenkinsfiles

---
+++ scripts/launch_aws_cluster.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import uuid
+import os
+import sys
+
+from passlib.hash import sha512_crypt
+
+from dcos_launch import config, get_launcher
+
+
+assert 'DCOS_TEST_INSTALLER_URL' in os.environ
+assert 'DCOS_TEST_LICENSE' in os.environ
+assert 'DCOS_TEST_SSH_KEY_PATH' in os.environ
+assert 'DCOS_TEST_ADMIN_USERNAME' in os.environ
+assert 'DCOS_TEST_ADMIN_PASSWORD' in os.environ
+
+dcos_launch_config = {
+    'launch_config_version': 1,
+    'deployment_name': "dcos-core-cli-e2e-tests-" + uuid.uuid4().hex,
+    'installer_url': os.environ['DCOS_TEST_INSTALLER_URL'],
+    'platform': 'aws',
+    'provider': 'onprem',
+    'aws_region': 'us-west-2',
+    'aws_key_name': 'default',
+    'ssh_private_key_filename': os.environ['DCOS_TEST_SSH_KEY_PATH'],
+    'os_name': 'cent-os-7-dcos-prereqs',
+    'instance_type': 'm4.large',
+    'num_masters': 1,
+    'num_private_agents': 1,
+    'num_public_agents': 1,
+    'dcos_config': {
+        'cluster_name': 'DC/OS Licensing CLI Integration Tests',
+        'resolvers': ['10.10.0.2'],
+        'dns_search': 'us-west-2.compute.internal',
+        'master_discovery': 'static',
+        'exhibitor_storage_backend': 'static',
+        'superuser_username': os.environ['DCOS_TEST_ADMIN_USERNAME'],
+        'superuser_password_hash': sha512_crypt.hash(os.environ['DCOS_TEST_ADMIN_PASSWORD']),
+        'fault_domain_enabled': False,
+        'license_key_contents': os.environ['DCOS_TEST_LICENSE'],
+    },
+}
+
+dcos_launch_config = config.get_validated_config(dcos_launch_config, '/tmp')
+
+launcher = get_launcher(dcos_launch_config)
+
+cluster_info = launcher.create()
+
+launcher = get_launcher(cluster_info)
+launcher.wait()
+
+# Workaround for `launcher.install_dcos()` printing to stdout.
+real_stdout = sys.stdout
+sys.stdout = open(os.devnull, "w")
+try:
+    launcher.install_dcos()
+finally:
+    sys.stdout.close()
+    sys.stdout = real_stdout
+
+master = next(iter(launcher.describe().get('masters')))
+
+print(master.get('public_ip'), end='')
---
+++ scripts/publish_binaries.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import sys
+
+import boto3
+import requests
+
+# TODO: the current DC/OS dev version (1.12) should be pulled dynamically (from the Github API?).
+version = os.environ.get("TAG_NAME") or "dcos-1.12"
+
+s3_client = boto3.resource('s3').meta.client
+bucket = "downloads.dcos.io"
+artifacts = [
+    ("linux/dcos", "binaries/cli/linux/x86-64/{}/dcos".format(version)),
+    ("darwin/dcos", "binaries/cli/darwin/x86-64/{}/dcos".format(version)),
+    ("windows/dcos.exe", "binaries/cli/windows/x86-64/{}/dcos.exe".format(version))
+]
+
+# TODO: this should probably passed as argument.
+build_path = os.path.dirname(os.path.realpath(__file__)) + "/../build"
+
+#for f, bucket_key in artifacts:
+#    s3_client.upload_file(build_path + "/" + f, bucket, bucket_key)
+
+slack_token = os.environ.get("SLACK_API_TOKEN")
+if not slack_token or version.startswith("dcos-"):
+    sys.exit(0)
+
+attachment_text = version + " has been published!"
+s3_urls = ["https://{}/{}".format(bucket, a[1]) for a in artifacts]
+
+try:
+    resp = requests.post(
+      "https://mesosphere.slack.com/services/hooks/jenkins-ci?token=" + slack_token,
+      json={
+        "channel": "#dcos-cli-ci",
+        "color": "good",
+        "attachments": [
+            {
+                "color": "good",
+                "title": "dcos-core-cli",
+                "text":  "\n".join([attachment_text + " :rocket:"] + s3_urls),
+                "fallback": "[dcos-core-cli] " + attachment_text
+            }
+        ]
+      }, timeout=30)
+
+    if resp.status_code != 200:
+        raise Exception("received {} status response: {}".format(resp.status_code, resp.text))
+except Exception as e:
+    print("Couldn't post Slack notification:\n  {}".format(e))
