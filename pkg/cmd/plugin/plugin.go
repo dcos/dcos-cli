80f1181a67c2151449a23f33ba128f8e4ae8c001
Bilal Amarni <bilal.amarni@gmail.com>
2018-07-23 12:10:03 +0200

    Implement "dcos plugin remove"
    It allows to remove a plugin from the filesystem based on its name.
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3834

---
+++ pkg/cmd/plugin/plugin.go
@@ -0,0 +1,18 @@
+package plugin
+
+import (
+	"github.com/dcos/dcos-cli/api"
+	"github.com/spf13/cobra"
+)
+
+// NewCommand creates the `dcos plugin` subcommand.
+func NewCommand(ctx api.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "plugin",
+		Short: "Manage CLI plugins",
+	}
+	cmd.AddCommand(
+		newCmdPluginRemove(ctx),
+	)
+	return cmd
+}
---
+++ pkg/cmd/plugin/plugin_remove.go
@@ -0,0 +1,22 @@
+package plugin
+
+import (
+	"github.com/dcos/dcos-cli/api"
+	"github.com/spf13/cobra"
+)
+
+// newCmdPluginRemove creates the `dcos plugin remove` subcommand.
+func newCmdPluginRemove(ctx api.Context) *cobra.Command {
+	return &cobra.Command{
+		Use:   "remove",
+		Short: "Remove a CLI plugin",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cluster, err := ctx.Cluster()
+			if err != nil {
+				return err
+			}
+			return ctx.PluginManager(cluster.SubcommandsDir()).Remove(args[0])
+		},
+	}
+}
