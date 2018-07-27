a412671cf0d145e7ec272ba75cba433477fb256d
Bilal Amarni <bilal.amarni@gmail.com>
2018-07-27 15:48:53 +0200

    Introduce the `dcos plugin add` command
    It relies on the plugin manager to install plugins.
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3833

---
+++ pkg/cmd/plugin/plugin_add.go
@@ -0,0 +1,25 @@
+package plugin
+
+import (
+	"github.com/dcos/dcos-cli/api"
+	"github.com/spf13/cobra"
+)
+
+// newCmdPluginAdd creates the `dcos plugin add` subcommand.
+func newCmdPluginAdd(ctx api.Context) *cobra.Command {
+	var update bool
+	cmd := &cobra.Command{
+		Use:   "add",
+		Short: "Add a CLI plugin",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cluster, err := ctx.Cluster()
+			if err != nil {
+				return err
+			}
+			return ctx.PluginManager(cluster).Install(args[0], update)
+		},
+	}
+	cmd.Flags().BoolVarP(&update, "update", "u", false, "")
+	return cmd
+}
