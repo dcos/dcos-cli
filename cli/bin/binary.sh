e5946a0c5a26adb112252dc869aceee9eaaecf41
Bilal Amarni <bilal.amarni@gmail.com>
2018-08-01 16:41:42 +0200

    Fix unbound variable bash error

--- cli/bin/binary.sh
+++ cli/bin/binary.sh
@@ -3,7 +3,7 @@
 CURRDIR=$(dirname "${0}")
 source ${CURRDIR}/common.sh
 
-if [ -n "${TAG_NAME}" ]; then
+if [ -n "${TAG_NAME-}" ]; then
     echo "Injecting CLI version."
     echo "version = \"${TAG_NAME}\"" > ${BUILDDIR}/../dcos/__init__.py
     echo "version = \"${TAG_NAME}\"" > ${BUILDDIR}/dcoscli/__init__.py
