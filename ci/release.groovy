f1c5ba96b5489e401f86722a467c80590150c2cc
Bilal Amarni <bilal.amarni@gmail.com>
2018-08-14 11:25:33 +0200

    Continuous delivery of the Go CLI to S3
    This sets up continuous delivery for the Go CLI.
    
    The master branch is continuously published to :
    
    - https://downloads.dcos.io/binaries/cli/linux/x86-64/latest/dcos
    - https://downloads.dcos.io/binaries/cli/darwin/x86-64/latest/dcos
    - https://downloads.dcos.io/binaries/cli/windows/x86-64/latest/dcos.exe
    
    Tags are released to :
    
    - https://downloads.dcos.io/binaries/cli/linux/x86-64/{tag}/dcos
    - https://downloads.dcos.io/binaries/cli/darwin/x86-64/{tag}/dcos
    - https://downloads.dcos.io/binaries/cli/windows/x86-64/{tag}/dcos.exe
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3955

---
+++ ci/release.groovy
@@ -0,0 +1,51 @@
+#!/usr/bin/env groovy
+
+pipeline {
+  agent none
+
+  options {
+    timeout(time: 6, unit: 'HOURS')
+  }
+
+  stages {
+    stage("Build binaries") {
+      agent { label 'mesos-ubuntu' }
+
+      steps {
+          sh 'make linux darwin windows'
+          stash includes: 'build/**', name: 'dcos-binaries'
+      }
+    }
+
+    stage("Release binaries to S3") {
+      when {
+        anyOf {
+          branch 'master'
+          expression { env.TAG_NAME != null }
+        }
+      }
+
+      agent { label 'py36' }
+
+      steps {
+        withCredentials([
+            string(credentialsId: "8b793652-f26a-422f-a9ba-0d1e47eb9d89", variable: "SLACK_API_TOKEN"),
+            string(credentialsId: "e270aa3f-4825-480c-a3ec-18a541c4e2d1",variable: "AWS_ACCESS_KEY_ID"),
+            string(credentialsId: "cd616d55-78eb-45de-b7a8-e5bc5ccce4c7",variable: "AWS_SECRET_ACCESS_KEY"),
+        ]) {
+            unstash 'dcos-binaries'
+
+            sh '''
+              bash -exc " \
+                cd ci; \
+                python -m venv env; \
+                source env/bin/activate; \
+                pip install --upgrade pip setuptools; \
+                pip install -r requirements.txt; \
+                ./release.py"
+            '''
+        }
+      }
+    }
+  }
+}
