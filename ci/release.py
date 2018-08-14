ce4db954c8ed9d11133d9367c269558404e03cdb
Bilal Amarni <bilal.amarni@gmail.com>
2018-08-14 11:25:33 +0200

    Continuous delivery of the Go CLI to S3
    This sets up continuous delivery for the Go CLI.
    
    The master branch is continuously published to :
    
    - https://downloads.dcos.io/binaries/cli/linux/x86-64/latest/dcos
    - https://downloads.dcos.io/binaries/cli/darwin/x86-64/latest/dcos
    - https://downloads.dcos.io/binaries/cli/windows/x86-64/latest/dcos.exe
    
    Tags are released to :
    
    - https://downloads.dcos.io/binaries/cli/linux/x86-64/{tag}/dcos
    - https://downloads.dcos.io/binaries/cli/darwin/x86-64/{tag}/dcos
    - https://downloads.dcos.io/binaries/cli/windows/x86-64/{tag}/dcos.exe
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3955

---
+++ ci/release.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import sys
+
+import boto3
+import requests
+
+version = os.environ.get("TAG_NAME") or "latest"
+
+s3_client = boto3.resource('s3', region_name='us-west-2').meta.client
+bucket = "downloads.dcos.io"
+artifacts = [
+    ("linux/dcos", "binaries/cli/linux/x86-64/{}/dcos".format(version)),
+    ("darwin/dcos", "binaries/cli/darwin/x86-64/{}/dcos".format(version)),
+    ("windows/dcos.exe", "binaries/cli/windows/x86-64/{}/dcos.exe".format(version))
+]
+
+# TODO: this should probably passed as argument.
+build_path = os.path.dirname(os.path.realpath(__file__)) + "/../build"
+
+for f, bucket_key in artifacts:
+    s3_client.upload_file(build_path + "/" + f, bucket, bucket_key)
+
+slack_token = os.environ.get("SLACK_API_TOKEN")
+if not slack_token or version == "latest":
+    sys.exit(0)
+
+attachment_text = version + " has been released!"
+s3_urls = ["https://{}/{}".format(bucket, a[1]) for a in artifacts]
+
+try:
+    resp = requests.post(
+      "https://mesosphere.slack.com/services/hooks/jenkins-ci?token=" + slack_token,
+      json={
+        "channel": "#dcos-cli-ci",
+        "color": "good",
+        "attachments": [
+            {
+                "color": "good",
+                "title": "dcos-cli",
+                "text":  "\n".join([attachment_text + " :rocket:"] + s3_urls),
+                "fallback": "[dcos-cli] " + attachment_text
+            }
+        ]
+      }, timeout=30)
+
+    if resp.status_code != 200:
+        raise Exception("received {} status response: {}".format(resp.status_code, resp.text))
+except Exception as e:
+    print("Couldn't post Slack notification:\n  {}".format(e))
