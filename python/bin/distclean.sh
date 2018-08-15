b69d0bde6d19d377e4fac48fc16d4184c2d6420a
Bilal Amarni <bilal.amarni@gmail.com>
2018-08-15 17:58:59 +0200

    Remove the legacy Python code
    This removes the Python codebase as it has been moved to
    https://github.com/dcos/dcos-core-cli.
    
    This adapts a few README accordingly. We should probably create a new
    EXTERNAL_LICENSES file as the current one is outdated.
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3590
    
    Co-authored-by: Bilal Amarni <bilal.amarni@gmail.com>
    Co-authored-by: Armand Grillet <armand.grillet@outlook.com>

--- python/bin/distclean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRDIR=$(dirname "${0}")
-source ${CURRDIR}/common.sh
-
-rm -rf ${BUILDDIR}/${DIST} \
-       ${BUILDDIR}/${DIST_DOCKER}
-echo "Deleted distribution artifacts."
--- python/lib/dcoscli/bin/test-binary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CURRDIR=$(dirname "${0}")
-source ${CURRDIR}/common.sh
-
-export PATH="${BUILDDIR}/${DIST}:${PATH}"
-source ${CURRDIR}/test.sh
