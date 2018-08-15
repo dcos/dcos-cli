9fb250b34aa6948ce5617cd72cf6fe08b17ef124
Bilal Amarni <bilal.amarni@gmail.com>
2018-08-15 17:58:59 +0200

    Remove the legacy Python code
    This removes the Python codebase as it has been moved to
    https://github.com/dcos/dcos-core-cli.
    
    This adapts a few README accordingly. We should probably create a new
    EXTERNAL_LICENSES file as the current one is outdated.
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3590
    
    Co-authored-by: Bilal Amarni <bilal.amarni@gmail.com>
    Co-authored-by: Armand Grillet <armand.grillet@outlook.com>

--- python/bin/doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRDIR=$(dirname "${0}")
-source ${CURRDIR}/common.sh
-
-echo "Building documentation..."
-${BUILDDIR}/${VENV}/${BIN}/sphinx-build${EXE} -W -b html pydoc pydoc/_build
-echo "Documentation built."
--- python/bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRDIR=$(dirname "${0}")
-source ${CURRDIR}/common.sh
-
-echo "Running tests..."
-TOXWORKDIR=${BUILDDIR}/${TOX} ${BUILDDIR}/${VENV}/${BIN}/tox${EXE}
-echo "Tests completed."
