523e4316ef30470e0892475110ec9af98a6069d6
Brian Lewis <keller@csh.rit.edu>
2018-08-28 16:30:09 +0200

    move gitignore into the os directories so go generate doesn't break by deafult and add -ignore=\.gitignore to the go-bindata call so it isn't packed as well

--- pkg/setup/default_plugin_darwin.go
+++ pkg/setup/default_plugin_darwin.go
@@ -1,3 +1,3 @@
 package setup
 
-//go:generate go-bindata -pkg setup -o bundled_plugins.gen.go -prefix "bundled_plugins/darwin" bundled_plugins/darwin/
+//go:generate go-bindata -pkg setup -o bundled_plugins.gen.go -ignore=\.gitignore -prefix "bundled_plugins/darwin" bundled_plugins/darwin/
--- pkg/setup/default_plugin_windows.go
+++ pkg/setup/default_plugin_windows.go
@@ -1,3 +1,3 @@
 package setup
 
-//go:generate go-bindata -pkg setup -o bundled_plugins.gen.go -prefix "bundled_plugins/windows" bundled_plugins/windows/
+//go:generate go-bindata -pkg setup -o bundled_plugins.gen.go -ignore=\.gitignore -prefix "bundled_plugins/windows" bundled_plugins/windows/
