fea6befa144e1e8b88cfdff00b04dcb7e5b12612
Brian Lewis <keller@csh.rit.edu>
2018-08-30 16:16:34 +0200

    Change the name of the stub files to bundled_plugins_stub_<os>.gen.go Rename default_plugin_stub to generate_bundle_stub to make it more clear what it does. Collapse the three os specific generator files into generate_bundle_prod which will always output the three separate artifacts that are filtered during build instead of doing it beforehand

--- pkg/setup/default_plugin_darwin.go
+++
@@ -1,5 +0,0 @@
-//+build withbundle
-
-package setup
-
-//go:generate go-bindata -pkg setup -o bundled_plugins.gen.go -ignore=\.gitignore -nometadata -tags "withbundle" -prefix "bundled_plugins" bundled_plugins/darwin/
