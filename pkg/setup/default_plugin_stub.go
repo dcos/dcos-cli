842f46f5c2baf69561707dedf88817e70ae62335
Brian Lewis <keller@csh.rit.edu>
2018-08-30 11:49:01 +0200

    correct some commenting

--- pkg/setup/default_plugin_stub.go
+++ pkg/setup/default_plugin_stub.go
@@ -5,3 +5,7 @@ package setup
 //go:generate go-bindata -pkg setup -o bundled_plugins_linux.gen.go -ignore=\.gitignore -nometadata -dev -prefix "bundled_plugins" bundled_plugins/linux/
 //go:generate go-bindata -pkg setup -o bundled_plugins_darwin.gen.go -ignore=\.gitignore -nometadata -dev -prefix "bundled_plugins" bundled_plugins/darwin/
 //go:generate go-bindata -pkg setup -o bundled_plugins_windows.gen.go -ignore=\.gitignore -nometadata -dev -prefix "bundled_plugins" bundled_plugins/windows/
+
+// This file generates windows, mac, and linux bundles because they each should contain different things
+// but they're debug so they don't actually contain the data and instead will point to the correct
+// directory.
