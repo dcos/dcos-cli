902ed8cb609b2d4d5bd0386daa60427f70778993
Brian Lewis <keller@csh.rit.edu>
2018-09-13 12:27:42 +0200

    finish dcos plugin

--- pkg/cmd/completion/completion.sh
+++ pkg/cmd/completion/completion.sh
@@ -429,9 +429,89 @@ _dcos_config_unset() {
 }
 
 _dcos_plugin() {
-    :
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local commands=("add" "list" "remove")
+    local flags=("--help")
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            *)
+                __dcos_handle_compreply "${commands[@]}"
+                ;;
+        esac
+        return
+    fi
+
+    __dcos_handle_subcommand
 }
 
+_dcos_plugin_add() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=("--help" "--update")
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            *) ;;
+        esac
+        return
+    fi
+}
+
+_dcos_plugin_list() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=("--help" "--json")
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            *) ;;
+        esac
+        return
+    fi
+}
+
+_dcos_plugin_remove() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=("--help")
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            *) ;;
+        esac
+        return
+    fi
+}
 
 _dcos() {
 	local i c=1 command
