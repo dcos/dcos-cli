d159ceced16d7d034e63849cc8214df307027d57
Bilal Amarni <bilal.amarni@gmail.com>
2018-10-16 19:26:42 +0200

    Update publish_plugins.py to use the new endpoints
    https://jira.mesosphere.com/browse/DCOS_OSS-4278

--- scripts/publish_plugins.py
+++ scripts/publish_plugins.py
@@ -6,7 +6,12 @@ import boto3
 
 from plugin.package_plugin import package_plugin
 
-version = os.environ.get("TAG_NAME")
+if os.environ.get("TAG_NAME"):
+    version =  os.environ.get("TAG_NAME")
+    stability = "releases"
+else:
+    version = os.environ.get("BRANCH_NAME")
+    stability = "testing"
 
 build_path = os.path.dirname(os.path.realpath(__file__)) + "/../build"
 
@@ -23,5 +28,5 @@ s3_client = boto3.resource('s3', region_name='us-west-2').meta.client
 
 for platform in platforms:
     zip_file = platform + "/dcos-core-cli.zip"
-    bucket_key = 'cli/plugins/dcos-core-cli/{}/{}/x86-64/dcos-core-cli.zip'.format(version, platform)
+    bucket_key = "cli/{}/plugins/dcos-core-cli/{}/x86-64/dcos-core-cli-{}.zip".format(stability, platform, version)
     s3_client.upload_file(build_path + "/" + zip_file, "downloads.dcos.io", bucket_key)
