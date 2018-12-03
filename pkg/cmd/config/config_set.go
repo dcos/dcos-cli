e24899029ca403449a3a008b933e2c8eae932648
Bilal Amarni <bilal.amarni@gmail.com>
2018-12-03 11:29:44 +0100

    Update some properties in the help documentation
    They were using underscores instead of dots.

--- pkg/cmd/config/config_set.go
+++ pkg/cmd/config/config_set.go
@@ -10,7 +10,7 @@ func newCmdConfigSet(ctx api.Context) *cobra.Command {
 	return &cobra.Command{
 		Use:   "set <name> <value>",
 		Short: "Add or set a property in the configuration file used for the current cluster",
-		Long:  "The properties that can be set are: core.dcos_url, core.dcos_acs_token, core.ssl_verify, core.timeout, core.ssh_user, core_ssh_proxy_ip, core.pagination, core.reporting, core.mesos_master_url, core_prompt_login",
+		Long:  "The properties that can be set are: core.dcos_url, core.dcos_acs_token, core.ssl_verify, core.timeout, core.ssh_user, core.ssh_proxy_ip, core.pagination, core.reporting, core.mesos_master_url, core.prompt_login",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cluster, err := ctx.Cluster()
