01fcaeda3a02a0777f4f85fd1f1931d36c274e65
Armand Grillet <armand.grillet@outlook.com>
2019-03-07 17:58:53 +0100

    Updated homemebrew formula for 0.7.11.

--- dcos-cli.rb
+++ dcos-cli.rb
@@ -1,8 +1,8 @@
 class DcosCli < Formula
   desc "The DC/OS command-line interface"
   homepage "https://docs.mesosphere.com/latest/cli/"
-  url "https://github.com/dcos/dcos-cli/archive/0.7.9.tar.gz"
-  sha256 "5bf909eee73566631e19f554160540b7d5aa4693d2d8858a057cd86bd2875b78"
+  url "https://github.com/dcos/dcos-cli/archive/0.7.11.tar.gz"
+  sha256 "401e8b3e6903c8347103ac52b8a573d6979f0136afb039e5924ad63f0c5e12d7"
 
   depends_on "go" => :build
   depends_on "go-bindata" => :build
@@ -12,7 +12,7 @@ class DcosCli < Formula
     ENV["GOPATH"] = buildpath
     ENV["NO_DOCKER"] = "1"
 
-    ENV["VERSION"] = "0.7.9"
+    ENV["VERSION"] = "0.7.11"
     ENV["GO_BUILD_TAGS"] = "corecli"
 
     bin_path = buildpath/"src/github.com/dcos/dcos-cli"
@@ -28,6 +28,6 @@ class DcosCli < Formula
 
   test do
     run_output = shell_output("#{bin}/dcos --version 2>&1")
-    assert_match "dcoscli.version=0.7.9", run_output
+    assert_match "dcoscli.version=0.7.11", run_output
   end
 end
