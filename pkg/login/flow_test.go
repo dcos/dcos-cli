dcos/dcos-cli

commit 0adca289f76f8570e374a313c4ad5e2254f9c3a1

Discard the local login server on non-desktop environment
For the `dcos-oidc-auth0` login provider, when the user operates the CLI
from a container or a remote machine, the local webserver will not run
on the same machine as the browser. Later on the browser will try to
send the token to eg. `localhost:43383`, which won't be reachable.

When this situation arises, we had designed the UI to fallback to
printing the login token for copy-pasting, but once the CLI has
successfully initiated the local web server, it doesn't accept input
from STDIN at all, so the user has no chance to copy-paste the token.

This commit updates the CLI to fallback to reading from STDIN when the
browser can't be opened.

Note: as an alternative, it would have been possible to concurrently
wait for data on both the local web server and STDIN, but the current
fix is simpler and covers the container / remote use-cases as expected.

https://jira.mesosphere.com/browse/DCOS_OSS-5591

pkg/login/flow.go
@@ -167,29 +167,39 @@ func (f *Flow) triggerMethod(provider *Provider) (acsToken string, err error) {
 				if provider.ID == DCOSOIDCAuth0 {
 					loginServer, err = loginserver.New(startFlowURL)
 					if err != nil {
-						return "", err
+						f.logger.Error(err)
+					} else {
+						startFlowURL = loginServer.StartFlowURL()
+						go loginServer.Start()
+						defer loginServer.Close()
 					}
-					startFlowURL = loginServer.StartFlowURL()
-					go loginServer.Start()
 				}
 
 				if err := f.openBrowser(startFlowURL); err != nil {
-					return "", err
-				}
+					// Being unable to open the browser is not critical, a message was prompted
+					// to the user with the URL they should go to in order to start the login flow.
+					f.logger.Info(err)
 
-				if loginServer != nil {
+					// However we close and don't use the web server in this scenario, as the CLI might
+					// be running on a non-desktop environment so the we server would be stuck waiting.
+					// Falling back to reading from STDIN is safer here.
+					//
+					// See https://jira.mesosphere.com/browse/DCOS_OSS-5591
+					loginServer.Close()
+				} else if loginServer != nil {
 					token = <-loginServer.Token()
-				}
 
-				if token == "" {
-					token = f.prompt.Input("Enter token from the browser: ")
-				} else {
 					// When the token comes from the local webserver there is no need to retry. In case of an
 					// error, retrying isn't needed (it can't be a typo, no copy-pasting was involved).
 					// Thus we raise the attempt counter to 3.
 					attempt = 3
 				}
 			}
+
+			if token == "" {
+				token = f.prompt.Input("Enter token from the browser: ")
+			}
+
 			f.interactive = true
 
 			// methodBrowserOIDCToken relies on a login token,
@@ -262,10 +272,12 @@ func (f *Flow) openBrowser(startFlowURL string) error {
 		}
 		startFlowURL = req.URL.String()
 	}
-	if err := f.opener.Open(startFlowURL); err != nil {
-		f.logger.Error(err)
-	}
+
 	msg := "If your browser didn't open, please follow this link:\n\n    %s\n\n"
 	fmt.Fprintf(f.errout, msg, startFlowURL)
+
+	if err := f.opener.Open(startFlowURL); err != nil {
+		return err
+	}
 	return nil
 }

pkg/login/flow_test.go
@@ -1,9 +1,22 @@
 package login
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
+	"github.com/dcos/dcos-cli/pkg/httpclient"
+	"github.com/dcos/dcos-cli/pkg/login/loginserver"
+	"github.com/dcos/dcos-cli/pkg/open"
+	"github.com/dcos/dcos-cli/pkg/prompt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -38,4 +51,100 @@ func TestSelectUIDPasswordProvider(t *testing.T) {
 	provider, err := flow.selectProvider(providers)
 	require.NoError(t, err)
 	require.True(t, provider == providers["login-provider-1"] || provider == providers["login-provider-2"])
+}
+
+func TestTriggerMethodOIDCWithLoginServer(t *testing.T) {
+	expectedLoginToken := "dummy_login_token"
+	expectedACSToken := "dummy_acs_token"
+
+	opener := open.OpenerFunc(func(resource string) error {
+
+		startFlowURL, err := url.Parse(resource)
+		require.NoError(t, err)
+
+		loginData := &loginserver.LoginData{
+			Token:     expectedLoginToken,
+			CSRFToken: startFlowURL.Query().Get("dcos_cli_csrf_token"),
+		}
+
+		var buf bytes.Buffer
+		err = json.NewEncoder(&buf).Encode(loginData)
+		require.NoError(t, err)
+
+		resp, err := httpclient.New("").Post(startFlowURL.Query().Get("redirect_uri"), "application/json", &buf)
+		require.NoError(t, err)
+		require.Equal(t, 200, resp.StatusCode)
+
+		return nil
+	})
+
+	ts := httptest.NewServer(mockLoginEndpoint(t, expectedLoginToken, expectedACSToken))
+	defer ts.Close()
+
+	logger := &logrus.Logger{Out: ioutil.Discard}
+
+	flow := NewFlow(FlowOpts{
+		Logger: logger,
+		Opener: opener,
+	})
+
+	flow.client = NewClient(httpclient.New(ts.URL), logger)
+
+	acsToken, err := flow.triggerMethod(defaultOIDCImplicitFlowProvider())
+	require.NoError(t, err)
+	require.Equal(t, expectedACSToken, acsToken)
+}
+
+func TestTriggerMethodOIDCWithoutLoginServer(t *testing.T) {
+
+	// We're simulating a container environment where the browser can't be opened.
+	// See https://jira.mesosphere.com/browse/DCOS_OSS-5591
+	opener := open.OpenerFunc(func(_ string) error {
+		return errors.New("couldn't open browser, I'm just a container")
+	})
+
+	expectedLoginToken := "dummy_login_token"
+	expectedACSToken := "dummy_acs_token"
+
+	ts := httptest.NewServer(mockLoginEndpoint(t, expectedLoginToken, expectedACSToken))
+	defer ts.Close()
+
+	logger := &logrus.Logger{Out: ioutil.Discard}
+
+	var in bytes.Buffer
+	in.WriteString(expectedLoginToken)
+
+	flow := NewFlow(FlowOpts{
+		Prompt: prompt.New(&in, ioutil.Discard),
+		Logger: logger,
+		Opener: opener,
+	})
+
+	flow.client = NewClient(httpclient.New(ts.URL), logger)
+
+	acsToken, err := flow.triggerMethod(defaultOIDCImplicitFlowProvider())
+	require.NoError(t, err)
+	require.Equal(t, expectedACSToken, acsToken)
+}
+
+func mockLoginEndpoint(t *testing.T, expectedLoginToken, acsToken string) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(defaultLoginEndpoint, func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "POST", req.Method)
+
+		var credentials Credentials
+		if err := json.NewDecoder(req.Body).Decode(&credentials); err != nil {
+			w.WriteHeader(401)
+			return
+		}
+		if !assert.Equal(t, expectedLoginToken, credentials.Token) {
+			w.WriteHeader(401)
+			return
+		}
+		var jwt JWT
+		jwt.Token = acsToken
+		err := json.NewEncoder(w).Encode(&jwt)
+		assert.NoError(t, err)
+	})
+	return mux
 }

pkg/login/login.go
@@ -12,6 +12,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const defaultLoginEndpoint = "/acs/api/v1/auth/login"
+
 var (
 	// ErrAuthDisabled is the error returned when attempting to get authentication providers
 	// from a cluster without authentication.
@@ -133,7 +135,7 @@ func (c *Client) sniffAuth(acsToken string) (*http.Response, error) {
 // Login makes a POST requests to the login endpoint with the given credentials.
 func (c *Client) Login(loginEndpoint string, credentials *Credentials) (string, error) {
 	if loginEndpoint == "" {
-		loginEndpoint = "/acs/api/v1/auth/login"
+		loginEndpoint = defaultLoginEndpoint
 	}
 
 	reqBody, err := json.Marshal(credentials)

pkg/login/loginserver/server.go
@@ -68,16 +68,11 @@ func New(startFlowURL string) (*LoginServer, error) {
 	u.RawQuery = q.Encode()
 	ls.startFlowURL = u.String()
 
-	return ls, nil
-}
-
-// Start starts the login server.
-func (ls *LoginServer) Start() error {
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"https://dcos.auth0.com"},
 	})
 
-	return http.Serve(ls.listener, c.Handler(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	ls.srv = &http.Server{Handler: c.Handler(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		var loginData LoginData
 
 		err := json.NewDecoder(req.Body).Decode(&loginData)
@@ -90,7 +85,19 @@ func (ls *LoginServer) Start() error {
 			return
 		}
 		ls.tokenCh <- loginData.Token
-	})))
+	}))}
+
+	return ls, nil
+}
+
+// Start starts the login server.
+func (ls *LoginServer) Start() error {
+	return ls.srv.Serve(ls.listener)
+}
+
+// Close closes the login server.
+func (ls *LoginServer) Close() error {
+	return ls.srv.Close()
 }
 
 // StartFlowURL returns the start flow URL for the login server based flow.

pkg/open/open.go
@@ -21,3 +21,11 @@ func NewOsOpener(logger *logrus.Logger) *OsOpener {
 		logger: logger,
 	}
 }
+
+// OpenerFunc is a func adapter for the Opener interface.
+type OpenerFunc func(string) error
+
+// Open invokes the OpenerFunc.
+func (f OpenerFunc) Open(resource string) error {
+	return f(resource)
+}

commit 241ef36aebd1fe3c84134767c0a668ec8a90199b

Fix nil pointer dereference
This fixes a regression introduced in #1502, which can cause a
panic on non-desktop environments with browser-based login flows
other than auth0.

https://jira.mesosphere.com/browse/DCOS-60349

.golangci.toml
@@ -35,7 +35,7 @@ enable = [
 line-length = 150
 
 [linters-settings.gocyclo]
-min-complexity = 22
+min-complexity = 23
 
 [linters-settings.govet]
 check-shadowing = false

pkg/login/flow.go
@@ -185,7 +185,9 @@ func (f *Flow) triggerMethod(provider *Provider) (acsToken string, err error) {
 					// Falling back to reading from STDIN is safer here.
 					//
 					// See https://jira.mesosphere.com/browse/DCOS_OSS-5591
-					loginServer.Close()
+					if loginServer != nil {
+						loginServer.Close()
+					}
 				} else if loginServer != nil {
 					token = <-loginServer.Token()
 

pkg/login/flow_test.go
@@ -97,34 +97,49 @@ func TestTriggerMethodOIDCWithLoginServer(t *testing.T) {
 
 func TestTriggerMethodOIDCWithoutLoginServer(t *testing.T) {
 
-	// We're simulating a container environment where the browser can't be opened.
-	// See https://jira.mesosphere.com/browse/DCOS_OSS-5591
-	opener := open.OpenerFunc(func(_ string) error {
-		return errors.New("couldn't open browser, I'm just a container")
-	})
+	testCases := []struct {
+		provider *Provider
+	}{
+		{
+			// We're simulating a container environment where the browser can't be opened.
+			// See https://jira.mesosphere.com/browse/DCOS_OSS-5591
+			defaultOIDCImplicitFlowProvider(),
+		},
+		{
+			// Non-regression test for a panic on browser login flows other than auth0.
+			// https://jira.mesosphere.com/browse/DCOS-60349
+			shibbolethLoginProvider(),
+		},
+	}
 
-	expectedLoginToken := "dummy_login_token"
-	expectedACSToken := "dummy_acs_token"
+	for _, tc := range testCases {
+		expectedLoginToken := "dummy_login_token"
+		expectedACSToken := "dummy_acs_token"
 
-	ts := httptest.NewServer(mockLoginEndpoint(t, expectedLoginToken, expectedACSToken))
-	defer ts.Close()
+		ts := httptest.NewServer(mockLoginEndpoint(t, expectedLoginToken, expectedACSToken))
+		defer ts.Close()
 
-	logger := &logrus.Logger{Out: ioutil.Discard}
+		logger := &logrus.Logger{Out: ioutil.Discard}
 
-	var in bytes.Buffer
-	in.WriteString(expectedLoginToken)
+		var in bytes.Buffer
+		in.WriteString(expectedLoginToken)
 
-	flow := NewFlow(FlowOpts{
-		Prompt: prompt.New(&in, ioutil.Discard),
-		Logger: logger,
-		Opener: opener,
-	})
+		opener := open.OpenerFunc(func(_ string) error {
+			return errors.New("couldn't open browser, I'm just a container")
+		})
 
-	flow.client = NewClient(httpclient.New(ts.URL), logger)
+		flow := NewFlow(FlowOpts{
+			Prompt: prompt.New(&in, ioutil.Discard),
+			Logger: logger,
+			Opener: opener,
+		})
 
-	acsToken, err := flow.triggerMethod(defaultOIDCImplicitFlowProvider())
-	require.NoError(t, err)
-	require.Equal(t, expectedACSToken, acsToken)
+		flow.client = NewClient(httpclient.New(ts.URL), logger)
+
+		acsToken, err := flow.triggerMethod(tc.provider)
+		require.NoError(t, err)
+		require.Equal(t, expectedACSToken, acsToken)
+	}
 }
 
 func mockLoginEndpoint(t *testing.T, expectedLoginToken, acsToken string) http.Handler {
@@ -147,4 +162,15 @@ func mockLoginEndpoint(t *testing.T, expectedLoginToken, acsToken string) http.H
 		assert.NoError(t, err)
 	})
 	return mux
+}
+
+func shibbolethLoginProvider() *Provider {
+	return &Provider{
+		ID:           "shib-integration-test",
+		Type:         OIDCImplicitFlow,
+		ClientMethod: methodBrowserOIDCToken,
+		Config: ProviderConfig{
+			StartFlowURL: "/login?redirect_uri=urn:ietf:wg:oauth:2.0:oob",
+		},
+	}
 }