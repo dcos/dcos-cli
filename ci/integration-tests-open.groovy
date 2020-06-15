a99eaa2d9378b8ed6d221c70b43352b8bb84cd28
Tomasz Janiszewski <janiszt@gmail.com>
2020-06-15 20:00:44 +0200

    Run OSS E2E tests

--- ci/integration-tests-open.groovy
+++ ci/integration-tests-open.groovy
@@ -2,47 +2,76 @@
 
 @Library("sec_ci_libs@v2-latest") _
 
-def master_branches = ["master", ] as String[]
+def master_branches = ["master",] as String[]
+
+def credentials = [
+        [$class           : 'AmazonWebServicesCredentialsBinding',
+         credentialsId    : 'a20fbd60-2528-4e00-9175-ebe2287906cf',
+         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
+        [$class       : 'FileBinding',
+         credentialsId: '23743034-1ac4-49f7-b2e6-a661aee2d11b',
+         variable     : 'CLI_TEST_SSH_KEY_PATH'],
+        [$class       : 'StringBinding',
+         credentialsId: '0b513aad-e0e0-4a82-95f4-309a80a02ff9',
+         variable     : 'DCOS_TEST_INSTALLER_URL'],
+        [$class       : 'StringBinding',
+         credentialsId: 'ca159ad3-7323-4564-818c-46a8f03e1389',
+         variable     : 'DCOS_TEST_LICENSE'],
+        [$class          : 'UsernamePasswordMultiBinding',
+         credentialsId   : '323df884-742b-4099-b8b7-d764e5eb9674',
+         usernameVariable: 'DCOS_TEST_DEFAULT_CLUSTER_USERNAME',
+         passwordVariable: 'DCOS_TEST_DEFAULT_CLUSTER_PASSWORD']
+]
 
 pipeline {
-  agent none
+    agent none
 
-  options {
-    timeout(time: 6, unit: 'HOURS')
-  }
-
-  stages {
-    stage("Authorization") {
-      steps {
-        user_is_authorized(master_branches, "8b793652-f26a-422f-a9ba-0d1e47eb9d89", "#mesosphere-dev")
-      }
-    }
-    
-    stage("Build binaries") {
-      agent { label 'mesos-ubuntu' }
-
-      steps {
-          sh 'make linux darwin windows'
-          stash includes: 'build/linux/**', name: 'dcos-linux'
-          stash includes: 'build/darwin/**', name: 'dcos-darwin'
-          stash includes: 'build/windows/**', name: 'dcos-windows'
-      }
+    options {
+        timeout(time: 6, unit: 'HOURS')
     }
 
-    stage("Launch DC/OS cluster") {
-      agent { label 'py36' }
+    stages {
+        stage("Authorization") {
+            steps {
+                user_is_authorized(master_branches, "8b793652-f26a-422f-a9ba-0d1e47eb9d89", "#mesosphere-dev")
+            }
+        }
 
-      steps {
-        withCredentials([
-          [$class: 'AmazonWebServicesCredentialsBinding',
-          credentialsId: 'a20fbd60-2528-4e00-9175-ebe2287906cf',
-          accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-          [$class: 'FileBinding',
-          credentialsId: '23743034-1ac4-49f7-b2e6-a661aee2d11b',
-          variable: 'DCOS_TEST_SSH_KEY_PATH']
-        ]) {
-            sh '''
+        stage("Build binaries") {
+            agent { label 'mesos-ubuntu' }
+
+            steps {
+                sh 'make linux darwin windows'
+                stash includes: 'build/linux/**', name: 'dcos-linux'
+                stash includes: 'build/darwin/**', name: 'dcos-darwin'
+                stash includes: 'build/windows/**', name: 'dcos-windows'
+            }
+        }
+
+        stage("Launch AWS Cluster") {
+            steps {
+                retry(5) {
+                    withCredentials(credentials) {
+                        script {
+                            master_ip = sh(script: '''
+                                export TF_VAR_variant=open; \
+                                export TF_VAR_custom_dcos_download_path=https://downloads.dcos.io/dcos/testing/master/dcos_generate_config.sh; \
+                                cd scripts && ./launch_aws_cluster.sh''', returnStdout: true).trim()
+                        }
+                        stash includes: 'scripts/**/*', name: 'terraform'
+                    }
+                }
+            }
+        }
+
+        stage("Launch DC/OS cluster") {
+            agent { label 'py36' }
+
+            steps {
+                withEnv(["DCOS_TEST_DEFAULT_CLUSTER_HOST=${master_ip}"]) {
+                    withCredentials(credentials) {
+                        sh '''
               bash -exc " \
                 cd tests; \
                 python3 -m venv env; \
@@ -51,43 +80,49 @@ pipeline {
                 flake8 integration; \
                 export DCOS_TEST_VARIANT=open; \
                 ./launch_cluster.py https://downloads.dcos.io/dcos/testing/master/dcos_generate_config.sh"
-            '''
-            stash includes: 'tests/test_cluster.env.sh', name: 'test-cluster'
+                '''
+                        stash includes: 'tests/test_cluster.env.sh', name: 'test-cluster'
+                    }
+                }
+            }
         }
-      }
-    }
 
-    stage("Run Linux integration tests") {
-      agent { label 'py36' }
+        stage("Run Linux integration tests") {
+            agent { label 'py36' }
 
-      steps {
-          unstash 'test-cluster'
-          unstash 'dcos-linux'
 
-          sh '''
-            bash -exc " \
-              export DCOS_CLI_EXPERIMENTAL_AUTOINSTALL_PACKAGE_CLIS=1; \
-              PATH=$PWD/build/linux:$PATH; \
-              cd tests; \
-              dcos cluster remove --all; \
-              python3 -m venv env; \
-              source env/bin/activate; \
-              source test_cluster.env.sh; \
-              pip install -U pip; \
-              pip install -r requirements.txt; \
-              pytest integration"
-          '''
-      }
-    }
+            steps {
 
-    stage("Run macOS integration tests") {
-      agent { label 'mac-hh-yosemite' }
+                withEnv(["DCOS_TEST_DEFAULT_CLUSTER_HOST=${master_ip}"]) {
+                    withCredentials(credentials) {
+                        unstash 'dcos-linux'
 
-      steps {
-          unstash 'test-cluster'
-          unstash 'dcos-darwin'
+                        sh '''
+                    bash -exc " \
+                      export DCOS_CLI_EXPERIMENTAL_AUTOINSTALL_PACKAGE_CLIS=1; \
+                      PATH=$PWD/build/linux:$PATH; \
+                      cd tests; \
+                      dcos cluster remove --all; \
+                      python3 -m venv env; \
+                      source env/bin/activate; \
+                      pip install -U pip; \
+                      pip install -r requirements.txt; \
+                      pytest integration"
+                  '''
+                    }
+                }
+            }
+        }
 
-          sh '''
+        stage("Run macOS integration tests") {
+            agent { label 'mac-hh-yosemite' }
+
+            steps {
+                withEnv(["DCOS_TEST_DEFAULT_CLUSTER_HOST=${master_ip}"]) {
+                    withCredentials(credentials) {
+                        unstash 'dcos-darwin'
+
+                        sh '''
             bash -exc " \
               export LC_ALL=en_US.UTF-8; \
               export PYTHONIOENCODING=utf-8; \
@@ -102,35 +137,39 @@ pipeline {
               pip install -r requirements.txt; \
               pytest integration"
           '''
-      }
-    }
-
-    stage("Run Windows integration tests") {
-      agent {
-        node {
-          label 'windows'
-          customWorkspace 'C:\\windows\\workspace'
+                    }
+                }
+            }
         }
-      }
 
-      steps {
-          unstash 'test-cluster'
-          unstash 'dcos-windows'
+        stage("Run Windows integration tests") {
+            agent {
+                node {
+                    label 'windows'
+                    customWorkspace 'C:\\windows\\workspace'
+                }
+            }
 
-          bat '''
+            steps {
+                withEnv(["DCOS_TEST_DEFAULT_CLUSTER_HOST=${master_ip}"]) {
+                    withCredentials(credentials) {
+                        unstash 'dcos-windows'
+
+                        bat '''
             bash -exc " \
               export PYTHONIOENCODING=utf-8; \
               export DCOS_CLI_EXPERIMENTAL_AUTOINSTALL_PACKAGE_CLIS=1; \
               PATH=$PWD/build/windows:$PATH; \
               cd tests; \
               dcos cluster remove --all; \
-              source test_cluster.env.sh; \
               python -m venv env; \
               env/Scripts/python -m pip install -U pip; \
               env/Scripts/pip install -r requirements.txt; \
               env/Scripts/pytest -vv integration"
           '''
-      }
+                    }
+                }
+            }
+        }
     }
-  }
 }
