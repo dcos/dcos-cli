6b552cfbbff2a751ebef1a783d0a9fa284b1db15
janisz <janiszt@gmail.com>
2020-07-28 17:20:04 +0200

    Update integration-tests.groovy

--- ci/integration-tests.groovy
+++ ci/integration-tests.groovy
@@ -38,7 +38,7 @@ pipeline {
       }
 
       steps {
-        user_is_authorized([] as String[], '8b793652-f26a-422f-a9ba-0d1e47eb9d89', '#dcos-cli-ci')
+        user_is_authorized(master_branches, "8b793652-f26a-422f-a9ba-0d1e47eb9d89", "#dcos-cli-ci")
       }
     }
 
